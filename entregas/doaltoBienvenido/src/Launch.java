34995d36ad5f76bed37e385a6f52e31db4c64440
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-09-19 18:26:47 +0200

    feat: clase launcher finalizada

---
+++ entregas/doaltoBienvenido/src/Launch.java
@@ -0,0 +1,6 @@
+public class Launch {
+    public static void main(String[] args) {
+        TelefonoEscacharrado telefonoEscacharrado = new TelefonoEscacharrado();
+        telefonoEscacharrado.iniciarJuego();
+    }
+}
