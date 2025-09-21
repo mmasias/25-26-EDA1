da08a6074c9325ff1a6c59514abbf87a17ecbc25
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-09-21 17:06:52 +0200

    Agregar lógica de generación de caracteres aleatorios

--- entregas/orejaPaula/src/retos/Reto001/Utils.java
+++ entregas/orejaPaula/src/retos/Reto001/Utils.java
@@ -5,5 +5,10 @@ public class Utils {
 public static Mensaje generarMensajeAleatorio(int longitud) {
     return null;
 }
+Random random = new Random();
+char[] chars = new char[longitud];
+for (int i = 0; i < longitud; i++) {
+    chars[i] = (char) ('A' + random.nextInt(26));
+}
 
 
