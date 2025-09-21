93e77e3abf2a24d3dc50b3897dda24469a4fbd7d
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-09-21 23:14:34 +0200

    feat: clases terminadas

---
+++ entregas/arceMarina/src/Niño.java
@@ -0,0 +1,37 @@
+public class Niño {
+    private String nombre;
+    private String pizarra;
+    private final int NUMERO_LETRAS = 26;
+    private final int CAMBIOS_MAXIMOS = 3;
+
+    public Niño(String nombre) {
+        this.nombre = nombre;
+        this.pizarra = "";
+    }
+
+    public void limpiarPizarra() {
+        pizarra = "";
+    }
+
+    public String escribirEnPizarra(String mensaje, java.util.Random random) {
+        pizarra = mensaje;
+
+        char[] chars = pizarra.toCharArray();
+        int cambioPizarra = random.nextInt(CAMBIOS_MAXIMOS);
+        for (int i = 0; i < cambioPizarra; i++) {
+            int posicion = random.nextInt(chars.length);
+            char nueva = (char) ('A' + random.nextInt(NUMERO_LETRAS));
+            chars[posicion] = nueva;
+        }
+        pizarra = new String(chars);
+        return pizarra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPizarra() {
+        return pizarra;
+    }
+}
---
+++ entregas/arceMarina/src/TelefonoDescacharrado.java
@@ -0,0 +1,94 @@
+public class TelefonoEscacharrado {
+    private final int MAXIMOS_ESPERANDO = 100;
+    private final int DURACION_LUDOTECA = 120;
+    private final int MINIMO_NIÑOS_JUEGO = 6;
+    private final int LONGITUD_MENSAJE = 10;
+    private final int TIEMPO_EXTRA = 2;
+    private final int MAXIMO_LLEGADAS = 3;
+    private final int MINUTOS_LLEGADA_RAPIDA = 10;
+    private final int MINUTOS_LLEGADA_LENTA = 30;
+    private final int MODULO_LLEGADA_LENTA = 3;
+
+    private ColaNiños cola;
+    private Niño[] esperando;
+    private int esperandoContador;
+    private int tiempo;
+    private int totalMinutos;
+    private int indiceNombreActual;
+    private String[] nombres;
+    private Mensaje texto;
+    private java.util.Random random;
+
+    public TelefonoEscacharrado() {
+        this.cola = new ColaNiños();
+        this.esperando = new Niño[MAXIMOS_ESPERANDO];
+        this.esperandoContador = 0;
+        this.tiempo = 0;
+        this.totalMinutos = DURACION_LUDOTECA;
+        this.indiceNombreActual = 0;
+
+        // 🔹 Lista de nombres
+        this.nombres = new String[]{
+            "Andres", "Beatriz", "Camilo", "Diana", "Esteban",
+            "Fernanda", "Guillermo", "Helena", "Ismael", "Julieta",
+            "Kevin", "Lorena", "Mateo", "Natalia", "Oscar",
+            "Paula", "Rafael", "Sofia", "Tomas", "Valeria"
+        };
+
+        this.texto = new Mensaje();
+        this.random = new java.util.Random();
+    }
+
+    public void iniciarJuego() {
+        while (tiempo < totalMinutos) {
+            if (tiempo < MINUTOS_LLEGADA_RAPIDA) {
+                int llegadas = random.nextInt(MAXIMO_LLEGADAS);
+                for (int i = 0; i < llegadas && indiceNombreActual < nombres.length; i++) {
+                    cola.añadirNiño(new Niño(nombres[indiceNombreActual++]));
+                }
+            } else if (tiempo < MINUTOS_LLEGADA_LENTA) {
+                if (random.nextBoolean() && indiceNombreActual < nombres.length && tiempo % MODULO_LLEGADA_LENTA == 0) {
+                    cola.añadirNiño(new Niño(nombres[indiceNombreActual++]));
+                }
+            }
+            if (cola.tamañoCola() >= MINIMO_NIÑOS_JUEGO) {
+                limpiarPizarras();
+                String mensaje = generarMensaje();
+                texto.mensajeLn("Juego en minuto " + tiempo + ":");
+                texto.mensajeLn("Mensaje original: " + mensaje);
+                String mensajeFinal = pasarMensaje(cola.obtenerNiñosCola(), cola.tamañoCola(), mensaje);
+                texto.mensajeLn("Mensaje final en la pizarra: " + mensajeFinal);
+                for (int i = 0; i < esperandoContador; i++) cola.añadirNiño(esperando[i]);
+                esperandoContador = 0;
+                tiempo += cola.tamañoCola() + TIEMPO_EXTRA;
+            } else {
+                tiempo++;
+            }
+        }
+    }
+
+    private void limpiarPizarras() {
+        texto.mensajeLn("Limpieza de pizarras");
+        Niño[] niños = cola.obtenerNiñosCola();
+        for (int i = 0; i < cola.tamañoCola(); i++) {
+            niños[i].limpiarPizarra();
+        }
+    }
+
+    private String generarMensaje() {
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String mensaje = "";
+        for (int i = 0; i < LONGITUD_MENSAJE; i++) {
+            mensaje = mensaje + letras.charAt(random.nextInt(letras.length()));
+        }
+        return mensaje;
+    }
+
+    private String pasarMensaje(Niño[] ninos, int cantidad, String mensaje) {
+        String mensajeActual = mensaje;
+        for (int i = 0; i < cantidad; i++) {
+            mensajeActual = ninos[i].escribirEnPizarra(mensajeActual, random);
+        }
+        return mensajeActual;
+    }
+}
