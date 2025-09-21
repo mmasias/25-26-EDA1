c78885fb1221069b9f960d726265686407984147
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2025-09-21 16:46:02 +0200

    Modifico error, eliminio los niños de la cola en todas las partidas

--- entregas/morenoSara/src/Ludoteca.java
+++ entregas/morenoSara/src/Ludoteca.java
@@ -18,10 +18,13 @@ public class Ludoteca {
             llegadaDeNinos(minuto);
 
             if (topeCola > 5) {
-                System.out.println("\nAisha empieza un juego con " + topeCola + " niños");
+                System.out.println("\n📌 Aisha empieza un juego con " + topeCola + " niños");
                 Juego.jugar(cola, topeCola, minuto);
 
-                // Pasar niños de la sala de espera a la cola
+                // Después del juego, la cola se vacía
+                topeCola = 0;
+
+                // Pasar niños que esperaban a la cola
                 for (int i = 0; i < topeEspera; i++) {
                     cola[topeCola++] = salaEspera[i];
                     System.out.println(salaEspera[i].getNombre() + " pasa a la cola después del juego");
@@ -35,7 +38,7 @@ public class Ludoteca {
         int llegadas = 0;
 
         if (minuto < 10) {
-            llegadas = random.nextInt(3); // 0, 1 o 2 niños
+            llegadas = random.nextInt(3); // 0,1,2 niños
         } else if (minuto < 30) {
             if (minuto % 3 == 0 && random.nextBoolean()) {
                 llegadas = 1;
@@ -54,6 +57,7 @@ public class Ludoteca {
         }
     }
 }
+
 
 
 
