0da3d9195dc9d485305352576572f68dde09ab0f
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-09-21 20:21:11 +0200

    creación de nuevos métodos

--- entregas/anayaAndrea/src/Lydia.java
+++ entregas/anayaAndrea/src/Lydia.java
@@ -7,4 +7,17 @@ public class Lydia {
         salaEspera = new ArrayList<>();
     }
 
+    public void recibirNino(Nino nino) {
+        salaEspera.add(nino);
+    }
+
+    public ArrayList<Nino> pasarNinos() {
+        ArrayList<Nino> copia = new ArrayList<>(salaEspera);
+        salaEspera.clear();
+        return copia;
+    }
+
+    public boolean tieneNinos() {
+        return !salaEspera.isEmpty();
+    }
 }
