d4ed7722d2614d2e0325ad155a07a9f3061d56ed
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-09-21 16:37:33 +0200

    Main terminado

--- entregas/orejaPaula/src/retos/Reto001/Main.java
+++ entregas/orejaPaula/src/retos/Reto001/Main.java
@@ -7,39 +7,41 @@ public class Main {
         Fila fila = new Fila();
         JuegoTelefono juego = new JuegoTelefono(fila);
         List<Nino> esperaLidia = new ArrayList<>();
-    }
-}
-Random random = new Random();
-int tiempo = 0;
-int tiempoTotal = 120;
-int contadorNinos = 1;
+        Random random = new Random();
+        int tiempo = 0;
+        int tiempoTotal = 120;
+        int contadorNinos = 1;
 
-while (tiempo < tiempoTotal) {
-    tiempo++;
-}
-if (tiempo < 10) {
-    int nuevos = random.nextInt(3);
-    for (int i = 0; i < nuevos; i++) {
-        if (fila.tamano() > 5) {
-            esperaLidia.add(new Nino("Niño" + contadorNinos++));
-        } else {
-            fila.agregarNino(new Nino("Niño" + contadorNinos++));
+        while (tiempo < tiempoTotal) {
+            if (tiempo < 10) {
+                int nuevos = random.nextInt(3);
+                for (int i = 0; i < nuevos; i++) {
+                    if (fila.tamano() > 5) {
+                        esperaLidia.add(new Nino("Niño" + contadorNinos++));
+                    } else {
+                        fila.agregarNino(new Nino("Niño" + contadorNinos++));
+                    }
+                }
+            } else if (tiempo < 30) {
+                if (tiempo % 3 == 0 && random.nextBoolean()) {
+                    if (fila.tamano() > 5) {
+                        esperaLidia.add(new Nino("Niño" + contadorNinos++));
+                    } else {
+                        fila.agregarNino(new Nino("Niño" + contadorNinos++));
+                    }
+                }
+            }
+
+            if (fila.tamano() > 5 && tiempo % 15 == 0) {
+                Mensaje mensaje = Utils.generarMensajeAleatorio(10);
+                System.out.println("\n--- Nuevo Juego ---");
+                System.out.println("Mensaje original: " + mensaje.obtenerTexto());
+                juego.jugar(mensaje, esperaLidia);
+            }
+
+            tiempo++;
         }
+
+        System.out.println("\nLa ludoteca cerró. Fin de la simulación.");
     }
 }
-else if (tiempo < 30) {
-    if (tiempo % 3 == 0 && random.nextBoolean()) {
-        if (fila.tamano() > 5) {
-            esperaLidia.add(new Nino("Niño" + contadorNinos++));
-        } else {
-            fila.agregarNino(new Nino("Niño" + contadorNinos++));
-        }
-    }
-}
-if (fila.tamano() > 5 && tiempo % 15 == 0) {
-    Mensaje mensaje = Utils.generarMensajeAleatorio(10);
-    System.out.println("\n--- Nuevo Juego ---");
-    System.out.println("Mensaje original: " + mensaje.obtenerTexto());
-    juego.jugar(mensaje, esperaLidia);
-}
-System.out.println("\nLa ludoteca cerró. Fin de la simulación.");
