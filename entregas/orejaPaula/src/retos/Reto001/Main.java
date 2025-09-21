b9b53075067ad3a4ff93158bb9e9920627549085
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-09-21 16:16:58 +0200

    Crear clase Main con inicialización de fila, juego y lista de espera

---
+++ entregas/orejaPaula/src/retos/Reto001/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Fila fila = new Fila();
+        JuegoTelefono juego = new JuegoTelefono(fila);
+        List<Nino> esperaLidia = new ArrayList<>();
+    }
+}
