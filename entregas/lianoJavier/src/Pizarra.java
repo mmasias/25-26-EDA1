83220c16b1e1097dbdcdd4390517e3284faafe16
midas@tower <javierlianorioz@alumnos.uneatlantico.es>
2025-09-21 17:27:46 +0200

    feat(lianoJavier): implement message handling methods in Pizarra
    Add private char array for message storage, setMensaje method to set the message, limpiar method to clear it, and update imprimir to print the message instead of throwing an exception.

--- entregas/lianoJavier/src/Pizarra.java
+++ entregas/lianoJavier/src/Pizarra.java
@@ -1,9 +1,17 @@
-
 public class Pizarra {
 
+    private char[] mensaje = new char[10];
+
+    public void setMensaje(char[] msg) {
+        mensaje = msg.clone();
+    }
+
+    public void limpiar() {
+        mensaje = new char[10];
+    }
+
     public void imprimir() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+        System.out.println(new String(mensaje));
     }
 
 }
