64b154c510f4da185bea9492da833208766b44db
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-09-21 16:44:18 +0200

    Crear clase Nino con atributos nombre y pizarrin y constructor

---
+++ entregas/orejaPaula/src/retos/Reto001/Nino.java
@@ -0,0 +1,9 @@
+public class Nino {
+    private String nombre;
+    private String pizarrin;
+
+    public Nino(String nombre) {
+        this.nombre = nombre;
+        this.pizarrin = "";
+    }
+}
