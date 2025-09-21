228867e9c3e91adb974c2d99ee9bca2a97ed4191
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-09-21 20:23:31 +0200

    añadimos método para que se añadan a la fila los niños de Lydia

--- entregas/anayaAndrea/src/Aisha.java
+++ entregas/anayaAndrea/src/Aisha.java
@@ -1,4 +1,17 @@
-package src;
+import java.util.ArrayList;
+
 public class Aisha {
-    
+    private ArrayList<Nino> fila;
+    private boolean jugando;
+
+    public Aisha() {
+        fila = new ArrayList<>();
+        jugando = false;
+    }
+
+    public void recibirNinos(ArrayList<Nino> nuevos) {
+        fila.addAll(nuevos);
+    }
+
+
 }
