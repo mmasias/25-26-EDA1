a3386ed821161828d858e09c3e1a9a35b95eab96
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-09-21 22:17:21 +0200

    Clase niño

---
+++ entregas/romeroLeslie/src/Nino.java
@@ -0,0 +1,21 @@
+public class Nino {
+    private int id;
+    private String pizarrin;
+
+    public Nino(int id) {
+        this.id = id;
+        this.pizarrin = "";
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String pizarrin() {
+        return pizarrin;
+    }
+
+    public void escribir(String mensaje) {
+        pizarrin = mensaje;
+    }
+}
