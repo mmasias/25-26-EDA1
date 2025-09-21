bf5c30d00bb37be4b5ac46a44938d1b1eaf2d92c
Jime-Cal <jimena.calderon@alumnos.uneatlantico.es>
2025-09-22 01:15:06 +0200

    Clase de GenerarMensaje completa

--- entregas/calderonJimena/src/GeneradorMensaje.java
+++ entregas/calderonJimena/src/GeneradorMensaje.java
@@ -1,8 +1,18 @@
-
 public class GeneradorMensaje {
+    private static final int LONGITUD_MENSAJE = 10;
+    private static final String ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public String generarMensaje() {
-
+        char[] mensaje = new char[LONGITUD_MENSAJE];
+        for (int i = 0; i < LONGITUD_MENSAJE; i++) {
+            int indice = aleatorio(0, ALFABETO.length() - 1);
+            mensaje[i] = ALFABETO.charAt(indice);
+        }
+        return new String(mensaje);
     }
 
+    private int aleatorio(int minimo, int maximo) {
+        return (int) (Math.random() * (maximo - minimo + 1)) + minimo;
+    }
 }
+
