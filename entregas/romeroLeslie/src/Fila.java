20a49d8463fc56cc7228ee856d226e42747be645
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-09-21 22:21:34 +0200

    Clase llegada y fila

---
+++ entregas/romeroLeslie/src/Fila.java
@@ -0,0 +1,61 @@
+class Fila {
+    private Nino[] datos;
+    private int frente;
+    private int fin;
+    private int size;
+
+    public Fila(int capacidad) {
+        datos = new Nino[capacidad];
+        frente = 0;
+        fin = 0;
+        size = 0;
+    }
+
+    public void encolar(Nino n) {
+        if (size == datos.length) {
+            Nino[] nuevo = new Nino[datos.length * 2];
+            for (int i = 0; i < size; i++) {
+                int indice = (frente + i);
+                if (indice >= datos.length) {
+                    indice = indice - datos.length; 
+                }
+                nuevo[i] = datos[indice];
+            }
+            datos = nuevo;
+            frente = 0;
+            fin = size;
+        }
+        datos[fin] = n;
+        fin++;
+        if (fin >= datos.length) fin = 0; 
+        size++;
+    }
+
+    public int tamano() {
+        return size;
+    }
+
+    public Nino[] extraerTodos() {
+        Nino[] res = new Nino[size];
+        for (int i = 0; i < size; i++) {
+            int indice = (frente + i);
+            if (indice >= datos.length) indice = indice - datos.length;
+            res[i] = datos[indice];
+        }
+        frente = 0;
+        fin = 0;
+        size = 0;
+        return res;
+    }
+
+    public void moverATo(Fila otra) {
+        for (int i = 0; i < size; i++) {
+            int indice = (frente + i);
+            if (indice >= datos.length) indice = indice - datos.length;
+            otra.encolar(datos[indice]);
+        }
+        frente = 0;
+        fin = 0;
+        size = 0;
+    }
+}
