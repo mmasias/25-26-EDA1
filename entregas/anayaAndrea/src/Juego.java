986f48feb4a3a59b39c57b502085c5714eabda82
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-09-21 20:30:34 +0200

    lógica para que el mensaje se imprima como se pide y su duración

--- entregas/anayaAndrea/src/Juego.java
+++ entregas/anayaAndrea/src/Juego.java
@@ -19,5 +19,17 @@ public class Juego {
         mensajeFinal = mensajeOriginal;
         for (int i = 0; i < fila.size(); i++) {
             mensajeFinal = Tiempo.deformar(mensajeFinal, random);
-  
+        }
+
+        duracion = fila.size() + 2;
+
+        System.out.println("Mensaje original: " + mensajeOriginal);
+        System.out.println("Mensaje final:    " + mensajeFinal);
+        System.out.println("Niños en la fila: " + fila.size());
+        System.out.println("Duración: " + duracion + " minutos\n");
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
 }
