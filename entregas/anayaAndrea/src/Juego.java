720247aff1f187bfd184cc0bd1b924667adb04bd
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-09-21 20:29:21 +0200

    En la clase Juego añadimos método que genera el mensaje con las condiciones

--- entregas/anayaAndrea/src/Juego.java
+++ entregas/anayaAndrea/src/Juego.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Juego {
-    
+    private ArrayList<Nino> fila;
+    private Random random;
+    private String mensajeOriginal;
+    private String mensajeFinal;
+    private int duracion;
+
+    public Juego(ArrayList<Nino> fila, Random random) {
+        this.fila = fila;
+        this.random = random;
+    }
+
+    public void jugar() {
+        mensajeOriginal = Tiempo.generarMensaje(random);
+
+        mensajeFinal = mensajeOriginal;
+        for (int i = 0; i < fila.size(); i++) {
+            mensajeFinal = Tiempo.deformar(mensajeFinal, random);
+  
 }
