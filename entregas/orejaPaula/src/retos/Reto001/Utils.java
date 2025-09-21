63602ebc5e711fdb81ce14f73243b16208c9906b
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-09-21 17:08:55 +0200

    Utils Terminado

--- entregas/orejaPaula/src/retos/Reto001/Utils.java
+++ entregas/orejaPaula/src/retos/Reto001/Utils.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
 public class Utils {
+    public static Mensaje generarMensajeAleatorio(int longitud) {
+        Random random = new Random();
+        char[] chars = new char[longitud];
+        for (int i = 0; i < longitud; i++) {
+            chars[i] = (char) ('A' + random.nextInt(26));
+        }
+        return new Mensaje(new String(chars));
+    }
 }
-public static Mensaje generarMensajeAleatorio(int longitud) {
-    return null;
-}
-Random random = new Random();
-char[] chars = new char[longitud];
-for (int i = 0; i < longitud; i++) {
-    chars[i] = (char) ('A' + random.nextInt(26));
-}
-return new Mensaje(new String(chars));
+
 
 
