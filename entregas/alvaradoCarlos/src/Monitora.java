9102122dd1612d6e4b5537baac6abb525b376302
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-09-21 19:36:07 +0200

    añadidos métodos vacios en Monitora y Niño

--- entregas/alvaradoCarlos/src/Monitora.java
+++ entregas/alvaradoCarlos/src/Monitora.java
@@ -8,5 +8,45 @@ public class Monitora {
         this.nombre = nombre;
         this.cantidadDeNiños = cantidadDeNiños;
     }
+
+    public void recibeNiños(int minuto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recibeNiños'");
+    }
+
+    public int cantidadDeNiños() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cantidadDeNiños'");
+    }
+
+    public Niño[] entregaNiños(Monitora monitora2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'entregaNiños'");
+    }
+
+    public void entregaPizarrin(Pizarra[] pizarrines, Niño[] niños) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'entregaPizarrin'");
+    }
+
+    public void limpiaPizarra(Pizarra pizarra2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'limpiaPizarra'");
+    }
+
+    public void pideLimpiarPizarrines(Pizarra[] pizarrines, Niño[] niños) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pideLimpiarPizarrines'");
+    }
+
+    public String escribeMensaje(Pizarra pizarra2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'escribeMensaje'");
+    }
+
+    public void terminaJuegoActual() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'terminaJuegoActual'");
+    }
     
 }
--- entregas/alvaradoCarlos/src/Niño.java
+++ entregas/alvaradoCarlos/src/Niño.java
@@ -1,3 +1,23 @@
 public class Niño {
+
+    public void recibeMensaje(String mensaje, Pizarra pizarra) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'recibeMensaje'");
+    }
+
+    public void muestraMensaje(String mensaje, Niño niño) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'muestraMensaje'");
+    }
+
+    public void escribeEnPizarra(Pizarra pizarra, String mensaje) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'escribeEnPizarra'");
+    }
+
+    public boolean tienePizarrin() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tienePizarrin'");
+    }
     
 }
