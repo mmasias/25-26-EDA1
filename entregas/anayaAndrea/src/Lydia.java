3ab2fec39cb5c4327e1fdcaf92a7c0525aee891f
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-09-21 20:20:11 +0200

    añadimos rol a Lydia

--- entregas/anayaAndrea/src/Lydia.java
+++ entregas/anayaAndrea/src/Lydia.java
@@ -1,4 +1,10 @@
-package src;
+import java.util.ArrayList;
+
 public class Lydia {
-    
+    private ArrayList<Nino> salaEspera;
+
+    public Lydia() {
+        salaEspera = new ArrayList<>();
+    }
+
 }
