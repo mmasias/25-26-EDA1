7f54e30438edd4c90dc46d28f25fec0dfcea5368
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-09-21 23:32:53 +0200

    Implementación de clase Niño con métodos para escribir, pasar mensaje y limpiar pizarrines

--- entregas/alvaradoCarlos/src/Niño.java
+++ entregas/alvaradoCarlos/src/Niño.java
@@ -1,33 +1,51 @@
 public class Niño {
 
-    public void recibeMensaje(String mensaje, Pizarra pizarra) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'recibeMensaje'");
+    private Pizarra pizarrin;
+
+    public Niño() {
+        this.pizarrin = null;
     }
 
-    public void muestraMensaje(String mensaje, Niño niño) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'muestraMensaje'");
+    public int recibeMensaje(String mensaje, Pizarra pizarrin) {
+        if (this.pizarrin != null) {
+            this.pizarrin.escribir(mensaje);
+            System.out.println("El niño recibió y escribió el mensaje: " + mensaje);
+        }
+        return 1;
     }
 
-    public void escribeEnPizarra(Pizarra pizarra, String mensaje) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'escribeEnPizarra'");
+    public int muestraMensaje(String mensaje, Niño siguiente) {
+        if (siguiente != null) {
+
+            siguiente.recibeMensaje(mensaje, siguiente.pizarrin);
+            System.out.println("El niño pasó el mensaje al siguiente niño");
+        } else {
+            System.out.println("No hay siguiente niño para pasar el mensaje");
+        }
+        return 1; 
+    }
+
+    public int escribeEnPizarra(Pizarra pizarra, String mensaje) {
+        if (pizarra != null) {
+            pizarra.escribir(mensaje);
+            System.out.println("El último niño escribió el mensaje en la pizarra del salón: " + mensaje);
+        }
+        return 1;
     }
 
     public boolean tienePizarrin() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tienePizarrin'");
+        return pizarrin != null;
     }
 
-    public void recibirPizarrin(Pizarra pizarra) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'recibirPizarrin'");
+    public void recibirPizarrin(Pizarra pizarrin) {
+        this.pizarrin = pizarrin;
     }
 
     public void limpiarPizarrin() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'limpiarPizarrin'");
+        if (pizarrin != null) {
+            pizarrin.limpiar();
+            System.out.println("El niño limpió su pizarrín");
+        }
     }
-    
+
 }
