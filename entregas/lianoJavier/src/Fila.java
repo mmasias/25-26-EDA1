1d6fda2507efde78f8dfefbf047c8ffdcf807a2d
midas@tower <javierlianorioz@alumnos.uneatlantico.es>
2025-09-21 17:18:06 +0200

    feat(fila): implement fila class methods
    Add agregar method to enqueue Ninho objects and increment size, update estaCompleto to check if size exceeds 5, and implement toArray method to convert queue to array while preserving order.

--- entregas/lianoJavier/src/Fila.java
+++ entregas/lianoJavier/src/Fila.java
@@ -1,9 +1,29 @@
-
 public class Fila {
 
-    public boolean estaCompleto() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'completo'");
+    private ColaNinho cola = new ColaNinho();
+    private int size = 0;
+
+    public void agregar(Ninho ninho) {
+        cola.enqueue(ninho);
+        size++;
     }
 
+    public boolean estaCompleto() {
+        return size > 5;
+    }
+
+    public Ninho[] toArray() {
+        Ninho[] array = new Ninho[size];
+        ColaNinho temp = new ColaNinho();
+        int i = 0;
+        while (!cola.estaVacia()) {
+            Ninho n = cola.dequeue();
+            array[i++] = n;
+            temp.enqueue(n);
+        }
+        while (!temp.estaVacia()) {
+            cola.enqueue(temp.dequeue());
+        }
+        return array;
+    }
 }
