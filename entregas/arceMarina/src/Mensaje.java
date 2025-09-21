9c7b87e790742eaca5eba6c62b5f7dd37aa398d6
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-09-21 23:03:22 +0200

    feat: clase Mensaje terminada

---
+++ entregas/arceMarina/src/Mensaje.java
@@ -0,0 +1,9 @@
+public class Mensaje {
+    public void mensajeLn(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public void mensaje(String mensaje) {
+        System.out.print(mensaje);
+    }
+}
