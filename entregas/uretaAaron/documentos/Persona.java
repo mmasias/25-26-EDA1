08160f5d3af67063acc7eca9e167c57b2effa911
AaronUreta <aaron.ureta@uneatlantico.es>
2025-09-22 17:11:49 +0200

    ultimos ajustes

--- entregas/uretaAaron/documentos/Persona.java
+++ entregas/uretaAaron/documentos/Persona.java
@@ -1,13 +1,9 @@
 public abstract class Persona {
     protected String nombre;
-
     public Persona(String nombreRecibido) {
         this.nombre = nombreRecibido;
     }
-
     public String obtenerNombre() {
-        String nombreResultado;
-        nombreResultado = this.nombre;
-        return nombreResultado;
+        return this.nombre;
     }
 }
