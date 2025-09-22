51bbd192f118e965a9d6f8d941a3a02c2ecc751d
chao.zhen@alumnos.uneatlantico.es <chao.zhen@alumnos.uneatlantico.es>
2025-09-21 17:42:52 -0700

    Creacion de la clase de Niño

---
+++ entregas/chaoZhen/src/Nino.java
@@ -0,0 +1,12 @@
+public class Nino {
+    private final String nombre;
+
+    public Nino(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+}
