d5198b9d4dff48db4cbfb14bba32ff06889bb6f4
AaronUreta <aaron.ureta@uneatlantico.es>
2025-09-22 16:59:08 +0200

    crear la clase trabajadora

--- entregas/uretaAaron/documentos/Persona.java
+++ entregas/uretaAaron/documentos/Persona.java
@@ -1,3 +1,13 @@
-public class Persona {
-    
+public abstract class Persona {
+    protected String nombre;
+
+    public Persona(String nombreRecibido) {
+        this.nombre = nombreRecibido;
+    }
+
+    public String obtenerNombre() {
+        String nombreResultado;
+        nombreResultado = this.nombre;
+        return nombreResultado;
+    }
 }
