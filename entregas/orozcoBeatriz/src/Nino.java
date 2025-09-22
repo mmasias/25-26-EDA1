cee0596e4d10ee0598f8f5ebe9aa5e55e3dbae6e
Bea <beatrizorozcodelgado@gmail.com>
2025-09-22 10:00:34 +0200

    Creo objeto Niño con nombre y pizarra

--- entregas/orozcoBeatriz/src/Nino.java
+++ entregas/orozcoBeatriz/src/Nino.java
@@ -1,3 +1,24 @@
 public class Nino {
-    
+    private String nombre;
+    private String mensajeEnPizarra = "";
+
+    public Nino(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void limpiarPizarra() {
+        mensajeEnPizarra = "";
+    }
+
+    public void escribirMensaje(String mensaje) {
+        mensajeEnPizarra = mensaje;
+    }
+
+    public String entregarMensaje() {
+        return mensajeEnPizarra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
