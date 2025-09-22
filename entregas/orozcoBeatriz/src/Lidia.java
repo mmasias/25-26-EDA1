5ce154edcfbe897bf07a805bca0558f86566a484
Bea <beatrizorozcodelgado@gmail.com>
2025-09-22 11:04:26 +0200

    creo objeto Lidia que recibe niños y los retiene si hay un juego en curso

--- entregas/orozcoBeatriz/src/Lidia.java
+++ entregas/orozcoBeatriz/src/Lidia.java
@@ -1,3 +1,22 @@
 public class Lidia {
-    
+    private Nino[] retenidos = new Nino[0];
+
+    public void recibirNino(Nino n, boolean juegoEnCurso) {
+        if (juegoEnCurso) {
+            Nino[] nuevo = new Nino[retenidos.length + 1];
+            for (int i = 0; i < retenidos.length; i++) {
+                nuevo[i] = retenidos[i];
+            }
+            nuevo[retenidos.length] = n;
+            retenidos = nuevo;
+        } else {
+            System.out.println("Lidia pasa a " + n.getNombre() + " con Aisha.");
+        }
+    }
+
+    public Nino[] liberarNinos() {
+        Nino[] copia = retenidos;
+        retenidos = new Nino[0];
+        return copia;
+    }
 }
