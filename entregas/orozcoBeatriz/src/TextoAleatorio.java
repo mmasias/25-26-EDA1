0504fd278a15918d98fc750593a78bca28600ad7
Bea <beatrizorozcodelgado@gmail.com>
2025-09-22 10:23:58 +0200

    Implemento un generador de textos aleatorios de 10 caracteres usando Random y código ASCII

--- entregas/orozcoBeatriz/src/TextoAleatorio.java
+++ entregas/orozcoBeatriz/src/TextoAleatorio.java
@@ -1,3 +1,15 @@
-public class TextoAleatorio {
+import java.util.Random;
 
+public class TextoAleatorio {
+    private Random random = new Random();
+
+    public String generar() {
+        int longitud = 10;
+        String resultado = "";
+        for (int i = 0; i < longitud; i++) {
+            char letra = (char) ('A' + random.nextInt(26));
+            resultado = resultado + letra;
+        }
+        return resultado;
+    }
 }
