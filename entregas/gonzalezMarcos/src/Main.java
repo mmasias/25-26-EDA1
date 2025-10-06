453b5c332ab58ef6f7b139c4321e88fd00e4aff7
Marcos González Hidalgo <marcos.gonzalez@alumnos.uneatlantico.es>
2025-10-06 12:30:11 +0200

    cambios en el codigo

--- entregas/gonzalezMarcos/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Juego telefonoEscacharrado = new Juego();
-        telefonoEscacharrado.iniciarJuego();
-    }
-}
---
+++ entregas/gonzalezMarcos/src/Niño.java
@@ -0,0 +1,52 @@
+package entregas.gonzalezMarcos.src;
+
+import utils.Console;
+
+import java.util.Random;
+
+class Niño {
+    private String nombre;
+    private Pizarra pizarrin;
+
+    public Niño(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void recibirPizarrin(Pizarra pizarra) {
+        this.pizarrin = pizarra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void recibirMensaje(String mensaje) {
+        pizarrin.escribirMensaje(modificarMensaje(mensaje));
+        new Console().writeln("[" + nombre + "] recibe [" + mensaje + "] y ha escrito [" + pizarrin.leerMensaje() + "]");        
+    }
+
+    private String modificarMensaje(String mensaje) {
+        Random random = new Random();
+        final String ALFABETO = "abcdefghijklmnopqrstuvwxyz";
+
+        int errores = random.nextInt(2) + 1;
+        
+        for (int i = 0; i < errores; i++) {
+            if (mensaje.length() > 0) {
+                int indiceAleatorio = random.nextInt(mensaje.length());
+                char letraAleatoria = ALFABETO.charAt(random.nextInt(ALFABETO.length()));
+                
+                mensaje = mensaje.substring(0, indiceAleatorio) + letraAleatoria + mensaje.substring(indiceAleatorio + 1);
+            }
+        }
+        return mensaje;
+    }
+
+    public String mostrarMensaje() {
+        return pizarrin.leerMensaje();
+    }
+
+    public void limpiarPizarrin() {
+        pizarrin.limpiar();
+    }
+}
