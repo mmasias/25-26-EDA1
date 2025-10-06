8019b944a47a58a6fe21d592cff45124b425b3d2
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 21:21:39 +0200

    mplementar método generarNiñosAleatorios para poblar Lidia

--- entregas/orejaPaula/src/Simulacion.java
+++ entregas/orejaPaula/src/Simulacion.java
@@ -2,3 +2,15 @@ import java.util.Random;
 
 public class Simulacion {
 }
+public static void generarNiñosAleatorios(Lidia lidia, int cantidad) {
+    String[] nombres = {"Ana","Luis","Carlos","Alberto","Aisha","Daisy","Miguel","Laura","Sofia","Andres",
+                        "Diego","Lucia","Martin","Paula","Alex","Valeria","Nicolas","Isabel","Julian","Emma"};
+    Random rand = new Random();
+    for (int i = 0; i < cantidad; i++) {
+        String nombre = nombres[rand.nextInt(nombres.length)];
+        int edad = rand.nextInt(9) + 2;
+        Niño n = new Niño(nombre, edad);
+        lidia.recibirNiño(n);
+        System.out.println("Llega " + n + " a la cola de Lidia");
+    }
+}
