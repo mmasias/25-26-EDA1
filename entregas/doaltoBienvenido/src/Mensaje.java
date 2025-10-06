8a8594bc12c322c968108dedfbf56bc0693969ff
Bienvenido Doalto Cavada <bienvenido.doalto@unealum.local>
2025-10-06 13:45:23 +0200

    feat: clase mensaje add

---
+++ entregas/doaltoBienvenido/src/Mensaje.java
@@ -0,0 +1,10 @@
+class Mensaje {
+
+    public void mensaje(String texto) {
+        System.out.print(texto);
+    }
+
+    public void mensajeLn(String texto) {
+        System.out.println(texto);
+    }
+}
