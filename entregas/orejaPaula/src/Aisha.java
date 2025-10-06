a72fd490fa367f6df40e5db35a90366f1c5f38b0
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:44:28 +0200

    Creación de la clase Aisha con atributos  y constructor

---
+++ entregas/orejaPaula/src/Aisha.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Aisha {
+    private String nombre;
+    private LinkedList<Niño> cola;
+
+    public Aisha() {
+        this.nombre = "Aisha";
+        this.cola = new LinkedList<>();
+    }
+}
+
+
