5e44f9eb4a03041ee9d80c552ad2e401e78dc4f1
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2025-10-06 20:17:31 +0200

    Clase Lydia

---
+++ entregas/morenoSara/src/Lydia.java
@@ -0,0 +1,11 @@
+public class Lydia extends Monitora {
+    public Lydia() {
+        super("Lydia");
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Lydia: Hola, soy Lydia, encargada de recibir a los niños");
+    }
+}
+
