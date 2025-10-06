911ebcc1321650957b9ae2263c7697abd1abd5d9
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:54:19 +0200

    Implementar método mostrarCola para listar los niños de Dalsy

--- entregas/orejaPaula/src/Dalsy.java
+++ entregas/orejaPaula/src/Dalsy.java
@@ -19,3 +19,13 @@ public LinkedList<Niño> getCola() {
 public int contarNiños() {
     return cola.size();
 }
+public void mostrarCola() {
+    if (cola.isEmpty()) {
+        System.out.println("  Cola vacía");
+    } else {
+        System.out.println("  Niños en cola: " + cola.size());
+        for (Niño n : cola) {
+            System.out.println("   - " + n);
+        }
+    }
+}
