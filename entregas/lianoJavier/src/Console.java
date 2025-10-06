3474e99aac64cf5e244d6698252fb89692f14048
midas@tower <javierlianorioz@alumnos.uneatlantico.es>
2025-10-06 22:45:53 +0200

    feat: implementar el método imprimirln, imprimir y linea en la clase Console

--- entregas/lianoJavier/src/Console.java
+++ entregas/lianoJavier/src/Console.java
@@ -1,9 +1,17 @@
 
 public class Console {
 
-        public static void imprimirln(String tiempo) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'imprimirln'");
+        public static void imprimirln(String mensaje) {
+                imprimir(mensaje);
+                linea();
+        }
+
+        public static void imprimir(String mensaje) {
+                System.out.print(mensaje);
+        }
+
+        public static void linea() {
+                System.out.println();
         }
 
 }
