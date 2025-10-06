14c77d9ff26aa6538f655dfe000901735f877740
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2025-10-06 13:24:24 +0200

    Creacion clase Mundo

---
+++ entregas/valdesCarlos/src/reto-002/Mundo.java
@@ -0,0 +1,16 @@
+
+
+class Mundo {
+    private Ludoteca ludoteca;
+    private Console console;
+    
+    public void ejecutarSimulacion() {
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = console.readInt("Seleccione opción: ");
+            procesarOpcion(opcion);
+            console.readString("Presione ENTER para continuar...");
+        } while (opcion != 0);
+    }
+}
