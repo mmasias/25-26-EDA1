a2504b0e385dd0b1c2292cbca1b690af4a8d5b94
samyx <samyxelita@gmail.com>
2025-10-06 22:53:37 +0200

    Archivos y logica de menu hecha

---
+++ entregas/apellidoNombre/Nino.java
@@ -0,0 +1,9 @@
+public class Nino {
+    String nombre;
+    int edad;   
+}
+
+
+
+
+
--- entregas/apellidoNombre/src/reto-002/Simulacion.java
+++
@@ -1,7 +0,0 @@
-class Simulacion {
-    public static void main(String[] args) {
-
-    Monitor Aisha = new Monitor("Aisha");
-    Monitor Aisha = new Monitor("Lydia");
-    }
-}
