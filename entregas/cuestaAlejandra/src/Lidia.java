ba2e362dc21ffea046bb767d6167a0677cc42711
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-10-06 13:28:58 +0200

    Lidia recibe a los niños, pasando lista

---
+++ entregas/cuestaAlejandra/src/Lidia.java
@@ -0,0 +1,9 @@
+class Lidia {
+    public List<Nino> recibirNinos() {
+        List<Nino> ninos = Arrays.asList(
+                new Nino("Maria", 3), new Nino("Luis", 6), new Nino("Aitor", 8), new Nino("Alejandra", 5),
+                new Nino("Carmen", 4), new Nino("Daniel", 7), new Nino("Lucía", 9), new Nino("Pablo", 2),
+                new Nino("Ismael", 10), new Nino("Marta", 5), new Nino("Alba", 6), new Nino("Iván", 4),
+                new Nino("Sara", 8), new Nino("Raúl", 3), new Nino("Nora", 7), new Nino("Mario", 9),
+                new Nino("Alex", 5), new Nino("Eva", 6), new Nino("Sergio", 2), new Nino("Paula", 10)
+        );
