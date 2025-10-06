83604b24a0d9d4877f8b09beda0f6ddc11f9a467
marcos@macbook <marcos.gonzalez@alumnos.uneatlantico.es>
2025-10-06 13:57:39 +0200

    refactor: Monitor.java

---
+++ entregas/gonzalezMarcos/src/Monitor.java
@@ -0,0 +1,59 @@
+public class Monitor {
+    private String nombre;
+    private Niño[] niños;
+    private int contador;  
+
+    public Monitor(String nombre) {
+        this.nombre = nombre;
+        this.niños = new Niño[50]; 
+        this.contador = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void recibeNiño(Niño n) {
+        if (contador < niños.length) {
+            niños[contador] = n;
+            contador++;
+        } else {
+            System.out.println("No se pueden añadir más niños a " + nombre);
+        }
+    }
+
+    public void entregaNiños(Monitor otro) {
+        for (int i = 0; i < contador; i++) {
+            otro.recibeNiño(niños[i]);
+        }
+        contador = 0; 
+    }
+
+    public Niño[] getNiños() {
+        Niño[] copia = new Niño[contador];
+        for (int i = 0; i < contador; i++) {
+            copia[i] = niños[i];
+        }
+        return copia;
+    }
+
+    public int numeroNiños() {
+        return contador;
+    }
+
+    public void mostrarListaNiños() {
+        Console c = new Console();
+        if (contador == 0) {
+            c.writeln(nombre + " no tiene niños actualmente.");
+            return;
+        }
+        c.writeln(nombre + " tiene a los siguientes niños:");
+        for (int i = 0; i < contador; i++) {
+            c.writeln("- " + niños[i]);
+        }
+    }
+
+    public void vaciar() {
+        contador = 0;
+    }
+}
