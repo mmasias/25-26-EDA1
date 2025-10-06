e39e396930d90a6d13d1e566a3c8f16dcb3a3e63
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-10-06 21:35:10 +0200

    Clase Pizarra

--- entregas/arceMarina/src/Pizarra.java
+++ entregas/arceMarina/src/Pizarra.java
@@ -1,25 +1,6 @@
-package entregas.arceMarina;
-
 class Pizarra {
-    private String mensaje;
-
-    public Pizarra() {
-        this.mensaje = "";
-    }
-
-    public void escribirMensaje(String mensaje) {
-        this.mensaje = mensaje;
-    }
-
-    public String leerMensaje() {
-        return this.mensaje;
-    }
-
-    public void limpiar() {
-        this.mensaje = "";
-    }
-
-    public boolean pizarraLimpia() {
-        return mensaje.length() <= 0;
-    }
+    private String mensaje = "";
+    public void escribirMensaje(String mensaje) { this.mensaje = mensaje; }
+    public String leerMensaje() { return mensaje; }
+    public void limpiar() { mensaje = ""; }
 }
