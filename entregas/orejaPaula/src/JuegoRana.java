0563d997337a03bcdec469f883b8d3bc60c762de
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:59:02 +0200

    Clase JuegoRana

--- entregas/orejaPaula/src/JuegoRana.java
+++ entregas/orejaPaula/src/JuegoRana.java
@@ -1,3 +1,4 @@
+```java
 public class JuegoRana {
     private Aisha aisha;
     private Dalsy dalsy;
@@ -6,18 +7,23 @@ public class JuegoRana {
         this.aisha = aisha;
         this.dalsy = dalsy;
     }
-}
-public boolean puedeJugar() {
-    return aisha.verificarJuegoRana();
-}
-public void iniciarJuego() {
-    aisha.separarParaJuego(dalsy);
-    System.out.println("Juego de la rana en progreso...");
-}
-public void finalizarJuego() {
-    for (Niño n : dalsy.getCola()) {
-        if (n.getEdad() < 5) aisha.recibirNiño(n);
+
+    public boolean puedeJugar() {
+        return aisha.verificarJuegoRana();
     }
-    dalsy.getCola().removeIf(n -> n.getEdad() < 5);
-    System.out.println("Juego de la rana terminado, todos los niños vuelven a Aisha");
+
+    public void iniciarJuego() {
+        aisha.separarParaJuego(dalsy);
+        System.out.println("Juego de la rana en progreso...");
+    }
+
+    public void finalizarJuego() {
+        for (Niño n : dalsy.getCola()) {
+            if (n.getEdad() < 5) aisha.recibirNiño(n);
+        }
+        dalsy.getCola().removeIf(n -> n.getEdad() < 5);
+        System.out.println("Juego de la rana terminado, todos los niños vuelven a Aisha");
+    }
 }
+```
+
