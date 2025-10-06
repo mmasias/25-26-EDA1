9dcf14e2618276972baea4b07f5fe8bdf09bcf94
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-10-06 13:23:26 +0200

    Crear clase niño

---
+++ entregas/cuestaAlejandra/src/Nino.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Nino {
+    String nombre;
+    int edad;
+
+    public Nino(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
