a550628f6d2bf2fe139c6cf7787d4907a6b0b9ad
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-10-06 22:14:49 +0200

    Implementada clase niño con atributos privados y metodos

---
+++ entregas/alvaradoCarlos/src/Niño.java
@@ -0,0 +1,20 @@
+
+public class Niño {
+
+    private String nombre;
+    private int edad;
+
+    public Niño(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String nombre() {
+        return nombre;
+    }
+
+    public int edad() {
+        return edad;
+    }
+
+}
