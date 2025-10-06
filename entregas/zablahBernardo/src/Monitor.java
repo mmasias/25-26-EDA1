3fe5a315dd1ac30a78726bd249e5ec62d22c9df1
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-10-06 23:11:23 +0200

    clase monitor

--- entregas/zablahBernardo/src/Monitor.java
+++ entregas/zablahBernardo/src/Monitor.java
@@ -1,128 +1,96 @@
-
-
 import utils.Console;
 
 class Monitor {
-    private static final int CAPACIDAD_MAXIMA = 15;
-    
+    private static final int CAPACIDAD_MAXIMA = 20;
     private String nombre;
-    private Niño[] niños;
+    private Niño[] colaNiños;
     private int inicio;
     private int fin;
     private int cantidad;
-    private boolean estaJugando;
-    private int turnoActual;
 
     public Monitor(String nombre) {
         this.nombre = nombre;
-        this.niños = new Niño[CAPACIDAD_MAXIMA];
-        this.inicio = 0;
-        this.fin = 0;
-        this.cantidad = 0;
-        this.estaJugando = false;
-        this.turnoActual = 0;
-    }
-
-    public void recibeNiño(Niño niño) {
-        if (cantidad >= CAPACIDAD_MAXIMA) {
-            new Console().writeln("ERROR: ¡" + nombre + " no puede recibir más niños! Cola llena.");
-            return;
-        }
-        
-        niños[fin] = niño;
-        fin = (fin + 1) % CAPACIDAD_MAXIMA;
-        cantidad++;
+        colaNiños = new Niño[CAPACIDAD_MAXIMA];
+        inicio = 0;
+        fin = 0;
+        cantidad = 0;
     }
 
     public boolean tieneNiños() {
         return cantidad > 0;
     }
 
-    public boolean puedeJugar() {
-        return cantidad >= 5;
+    public void recibirNiño(Niño niño) {
+        if (cantidad >= CAPACIDAD_MAXIMA) return;
+        colaNiños[fin] = niño;
+        fin = (fin + 1) % CAPACIDAD_MAXIMA;
+        cantidad++;
     }
 
-    public boolean estaJugando() {
-        return estaJugando;
-    }
-
-    public void mostrarListaNiños() {
-        new Console().write("> " + this.nombre + " --> ");
-        int actual = inicio;
+    public Niño[] obtenerTodosNiños() {
+        Niño[] lista = new Niño[cantidad];
+        int posicionActual = inicio;
         for (int i = 0; i < cantidad; i++) {
-            new Console().write(niños[actual].getNombre() + " / ");
-            actual = (actual + 1) % CAPACIDAD_MAXIMA;
+            lista[i] = colaNiños[posicionActual];
+            posicionActual = (posicionActual + 1) % CAPACIDAD_MAXIMA;
         }
-        new Console().writeln();
+        return lista;
     }
 
-    private void recibeNiño(Niño niño, Pizarra pizarrin) {
-        niño.recibirPizarrin(pizarrin);
-        recibeNiño(niño);
-    }
-
-    public void entregaNiños(Monitor otroMonitor) {
-        while (tieneNiños()) {
-            new Console().writeln(" >  " + this.nombre + " ENTREGA NIÑO");
-            Niño unNiño = sacarNiño();
+    public void vaciarCola() {
+        for (int i = 0; i < cantidad; i++) {
+            colaNiños[i] = null;
         }
+        inicio = 0;
+        fin = 0;
+        cantidad = 0;
     }
 
-    private Niño sacarNiño() {
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void mostrarListaNiños(Console console) {
+        console.writeln(nombre + ":");
         if (cantidad == 0) {
-            return null;
-        }
-        
-        Niño saliente = niños[inicio];
-        niños[inicio] = null;
-        inicio = (inicio + 1) % CAPACIDAD_MAXIMA;
-        cantidad--;
-        return saliente;
-    }
-
-    private Niño obtenerNiño(int posicion) {
-        if (posicion >= cantidad) {
-            return null;
-        }
-        int indice = (inicio + posicion) % CAPACIDAD_MAXIMA;
-        return niños[indice];
-    }
-
-    public void jugar() {
-        if (!estaJugando) {
-            estaJugando = true;
-            limpiarPizarrines();
-            turnoActual = 0;
-            Niño primerNiño = obtenerNiño(turnoActual);
-            if (primerNiño != null) {
-                primerNiño.recibirMensaje("ABCDEFGHIJKLM");
-            }
+            console.writeln("  Cola vacía");
         } else {
-            Niño niñoActual = obtenerNiño(turnoActual);
-            
-            if (turnoActual + 1 >= cantidad) {
-                estaJugando = false;
-                turnoActual = 0;
+            console.writeln("  Niños en cola: " + cantidad);
+            int posicionActual = inicio;
+            for (int i = 0; i < cantidad; i++) {
+                console.writeln("  - " + colaNiños[posicionActual].getNombre() + " (" + colaNiños[posicionActual].getEdad() + " años)");
+                posicionActual = (posicionActual + 1) % CAPACIDAD_MAXIMA;
+            }
+        }
+        console.writeln();
+    }
+    public double calcularEdadPromedio() {
+        if (cantidad == 0) return 0;
+        int suma = 0;
+        for (Niño niñoActual : obtenerTodosNiños()) {
+            suma += niñoActual.getEdad();
+        }
+        return (double)suma / cantidad;
+    }
+    public Niño[] separarMenoresDe(int edadMinima, Monitor otroMonitor) {
+        Niño[] quedan = new Niño[cantidad];
+        int contadorQuedan = 0;
+        Niño[] todos = obtenerTodosNiños();
+        vaciarCola();
+        for (Niño niñoActual : todos) {
+            if (niñoActual.getEdad() < edadMinima) {
+                otroMonitor.recibirNiño(niñoActual);
             } else {
-                Niño siguienteNiño = obtenerNiño(turnoActual + 1);
-                if (niñoActual != null && siguienteNiño != null) {
-                    siguienteNiño.recibirMensaje(niñoActual.mostrarMensaje());
-                }
-                turnoActual++;
+                recibirNiño(niñoActual);
+                quedan[contadorQuedan] = niñoActual;
+                contadorQuedan++;
             }
         }
-    }
-
-    private void limpiarPizarrines() {
-        for (int i = 0; i < cantidad; i++) {
-            Niño niño = obtenerNiño(i);
-            if (niño != null) {
-                niño.limpiarPizarrin();
-            }
+        Niño[] resultado = new Niño[contadorQuedan];
+        for (int i = 0; i < contadorQuedan; i++) {
+            resultado[i] = quedan[i];
         }
+        return resultado;
     }
-
-    public String getUsoMemoria() {
-        return cantidad + "/" + CAPACIDAD_MAXIMA + " (¡fijo!)";
-    }
+    
 }
