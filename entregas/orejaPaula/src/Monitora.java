545806195135fcb12bd7993e2fbf6d37eff2e871
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 21:09:38 +0200

    Añadir constructor para inicializar nombre y cola

--- entregas/orejaPaula/src/Monitora.java
+++ entregas/orejaPaula/src/Monitora.java
@@ -5,3 +5,7 @@ public abstract class Monitora {
     protected String nombre;
     protected LinkedList<Niño> cola;
 }
+public Monitora(String nombre) {
+    this.nombre = nombre;
+    this.cola = new LinkedList<>();
+}
