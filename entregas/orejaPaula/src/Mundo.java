b23fb82c9a16fd7cbb789289088f1a21776e06e1
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 21:15:23 +0200

    Implementar bucle principal de la simulación con menú y procesamiento de opciones

--- entregas/orejaPaula/src/Mundo.java
+++ entregas/orejaPaula/src/Mundo.java
@@ -8,4 +8,16 @@ public Mundo() {
     ludoteca = new Ludoteca();
     sc = new Scanner(System.in);
 }
+public void ejecutarSimulacion() {
+    int opcion;
+    do {
+        mostrarMenu();
+        opcion = leerInt("Seleccione una opción: ");
+        procesarOpcion(opcion);
+        if (opcion != 0) {
+            System.out.println("\nPresione ENTER para continuar...");
+            sc.nextLine();
+        }
+    } while (opcion != 0);
+}
 
