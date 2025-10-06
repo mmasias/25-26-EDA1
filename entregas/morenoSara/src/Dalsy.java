5474263e3918b3a5a44cf589b700a8707d1eae8f
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2025-10-06 20:17:55 +0200

    Clase Dalsy

---
+++ entregas/morenoSara/src/Dalsy.java
@@ -0,0 +1,10 @@
+public class Dalsy extends Monitora {
+    public Dalsy() {
+        super("Dalsy");
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Dalsy: Hola, soy Dalsy, cuido a los niños menores de 5 años");
+    }
+}
