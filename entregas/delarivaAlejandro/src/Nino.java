55e5c482272e9079dcda53570d1c0309fc8f9eda
Alejandro De La Riva Garcia <alejandro.delariva@alumnos.uneatlantico.es>
2025-10-06 14:02:07 +0200

    Clase Nino.java; nombre, edad y representación en texto.

---
+++ entregas/delarivaAlejandro/src/Nino.java
@@ -0,0 +1,22 @@
+public class Nino {
+    private final String nombre;
+    private final int edad;
+
+    public Ninio(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + edad + " años)";
+    }
+}
