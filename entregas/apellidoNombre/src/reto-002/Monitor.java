5109747a17ab573d48c3ece9496260508944cb9d
Samuel Alejandro Herrera Castillo <samuel.herrera@unealum.local>
2025-10-06 13:58:17 +0200

    Agregar archivos y clases iniciales

---
+++ entregas/apellidoNombre/src/reto-002/Monitor.java
@@ -0,0 +1,12 @@
+public class Monitor {
+    private String nombre;
+
+    public Monitor(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public void presentarse(){
+        System.out.println("Hola me llamo" + nombre);
+        
+    } 
+}
---
+++ entregas/apellidoNombre/src/reto-002/Simulacion.java
@@ -0,0 +1,7 @@
+class Simulacion {
+    public static void main(String[] args) {
+
+    Monitor Aisha = new Monitor("Aisha");
+    Monitor Aisha = new Monitor("Lydia");
+    }
+}
