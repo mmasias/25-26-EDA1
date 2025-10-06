571616baff23e74a38ef9427638525b740892def
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:52:51 +0200

    Crear la clase Dalsy con atributos y constructor básico

---
+++ entregas/orejaPaula/src/Dalsy.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class Dalsy {
+    private String nombre;
+    private LinkedList<Niño> cola;
+
+    public Dalsy() {
+        this.nombre = "Dalsy";
+        this.cola = new LinkedList<>();
+    }
+}
