a42032679b4d0927074b6fe7ce536e134f812d06
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 21:02:50 +0200

    Clase Lidia

--- entregas/orejaPaula/src/Lidia.java
+++ entregas/orejaPaula/src/Lidia.java
@@ -1,3 +1,4 @@
+```java
 import java.util.LinkedList;
 
 public class Lidia {
@@ -8,42 +9,49 @@ public class Lidia {
         this.nombre = "Lidia";
         this.cola = new LinkedList<>();
     }
-}
-public void recibirNiño(Niño n) {
-    cola.add(n);
-}
-public LinkedList<Niño> getCola() {
-    return cola;
-}
 
-public int contarNiños() {
-    return cola.size();
-}
-public void mostrarCola() {
-    if (cola.isEmpty()) {
-        System.out.println("  Cola vacía");
-    } else {
-        System.out.println("  Niños en cola: " + cola.size());
-        for (Niño n : cola) {
-            System.out.println("   - " + n);
+    public void recibirNiño(Niño n) {
+        cola.add(n);
+    }
+
+    public LinkedList<Niño> getCola() {
+        return cola;
+    }
+
+    public int contarNiños() {
+        return cola.size();
+    }
+
+    public void mostrarCola() {
+        if (cola.isEmpty()) {
+            System.out.println("  Cola vacía");
+        } else {
+            System.out.println("  Niños en cola: " + cola.size());
+            for (Niño n : cola) {
+                System.out.println("   - " + n);
+            }
+        }
+    }
+
+    public void transferirTodosA(Aisha aisha) {
+        while (!cola.isEmpty()) {
+            aisha.recibirNiño(cola.poll());
+        }
+    }
+
+    public void pasarNiñosAAisha(Aisha aisha) {
+        if (cola.size() >= 5) {
+            System.out.println("Lidia transfiere sus niños a Aisha:");
+            for (Niño n : cola) {
+                System.out.println(" - " + n);
+            }
+            transferirTodosA(aisha);
+        } else {
+            System.out.println("No hay suficientes niños para iniciar el juego");
+            System.out.println("Se necesitan al menos 5 niños");
         }
     }
 }
-public void transferirTodosA(Aisha aisha) {
-    while (!cola.isEmpty()) {
-        aisha.recibirNiño(cola.poll());
-    }
-}
-public void pasarNiñosAAisha(Aisha aisha) {
-    if (cola.size() >= 5) {
-        System.out.println("Lidia transfiere sus niños a Aisha:");
-        for (Niño n : cola) {
-            System.out.println(" - " + n);
-        }
-        transferirTodosA(aisha);
-    } else {
-        System.out.println("No hay suficientes niños para iniciar el juego");
-        System.out.println("Se necesitan al menos 5 niños");
-    }
-}
+```
+
 
