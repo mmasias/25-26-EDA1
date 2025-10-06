dda4f0cdf722bc1129ce89af467c10df78554571
Alejandro De La Peña <alejandro.delapena@alumnos.uneatlantico.es>
2025-10-06 13:45:24 +0200

    01. Creada Clase Niño con nombre, edad y método de presentación.

--- entregas/deLaPeñaAlejandro/src/Niños.java
+++ entregas/deLaPeñaAlejandro/src/Niños.java
@@ -1,1 +1,32 @@
+public class Ninio {
+    private String nombre;
+    private int edad;
+
+    public Ninio(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void presentarse() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años");
+    }
+
+    public boolean nombreEmpiezaPor(char letra) {
+        char inicial = Character.toUpperCase(nombre.charAt(0));
+        return inicial == Character.toUpperCase(letra);
+    }
+
+    public String toString() {
+        return nombre + " (" + edad + " años)";
+    }
+}
+
 
