fac501fc52e5a890e7aa2a492f79cf1420af1cf9
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-10-06 21:35:25 +0200

    clase Niño

--- entregas/arceMarina/src/Niño.java
+++ entregas/arceMarina/src/Niño.java
@@ -1,52 +1,41 @@
-package entregas.arceMarina;
-
-import utils.Console;
-
+import utils.Scanner;
 import java.util.Random;
 
 class Niño {
     private String nombre;
+    private int edad;
     private Pizarra pizarrin;
 
-    public Niño(String nombre) {
+    public Niño(String nombre, int edad) {
         this.nombre = nombre;
+        this.edad = edad;
+        this.pizarrin = new Pizarra();
     }
 
-    public void recibirPizarrin(Pizarra pizarra) {
-        this.pizarrin = pizarra;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
+    public String getNombre() { return nombre; }
+    public int getEdad() { return edad; }
 
     public void recibirMensaje(String mensaje) {
         pizarrin.escribirMensaje(modificarMensaje(mensaje));
-        new Console().writeln("[" + nombre + "] recibe [" + mensaje + "] y ha escrito [" + pizarrin.leerMensaje() + "]");        
+        System.out.println("[" + nombre + "] recibe [" + mensaje + "] y ha escrito [" + pizarrin.leerMensaje() + "]");
     }
 
     private String modificarMensaje(String mensaje) {
         Random random = new Random();
         final String ALFABETO = "abcdefghijklmnopqrstuvwxyz";
-
         int errores = random.nextInt(2) + 1;
-        
         for (int i = 0; i < errores; i++) {
             if (mensaje.length() > 0) {
-                int indiceAleatorio = random.nextInt(mensaje.length());
-                char letraAleatoria = ALFABETO.charAt(random.nextInt(ALFABETO.length()));
-                
-                mensaje = mensaje.substring(0, indiceAleatorio) + letraAleatoria + mensaje.substring(indiceAleatorio + 1);
+                int idx = random.nextInt(mensaje.length());
+                char letra = ALFABETO.charAt(random.nextInt(ALFABETO.length()));
+                mensaje = mensaje.substring(0, idx) + letra + mensaje.substring(idx + 1);
             }
         }
         return mensaje;
     }
 
-    public String mostrarMensaje() {
-        return pizarrin.leerMensaje();
-    }
-
-    public void limpiarPizarrin() {
-        pizarrin.limpiar();
-    }
+    public String mostrarMensaje() { return pizarrin.leerMensaje(); }
+    public void limpiarPizarrin() { pizarrin.limpiar(); }
+    public void presentarse() { System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años"); }
+    public void presentarseNombre() { System.out.println("Hola, soy " + nombre); }
 }
