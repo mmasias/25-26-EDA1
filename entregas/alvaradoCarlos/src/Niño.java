0767b7cafb3d880fecfd9b71e70366d7cd6ddb5a
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-10-06 23:42:18 +0200

    Implementación de presentación para siguiente niño en clase NIño

--- entregas/alvaradoCarlos/src/Niño.java
+++ entregas/alvaradoCarlos/src/Niño.java
@@ -3,10 +3,23 @@ public class Niño {
 
     private String nombre;
     private int edad;
+    private Niño siguienteNiño;
 
     public Niño(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
+        this.siguienteNiño = null;
+    }
+
+    public void presentarse(){
+        System.out.println(nombre + ": Hola, soy " + nombre + " y tengo " + edad + " años");
+        if (siguienteNiño != null) {
+            siguienteNiño.presentarse();
+        }
+    }
+
+    public void siguienteNiño(Niño siguiente) {
+        this.siguienteNiño = siguiente;
     }
 
     public String nombre() {
