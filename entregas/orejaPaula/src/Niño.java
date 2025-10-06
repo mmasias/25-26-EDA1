30e145689b88cd0387f1c562a43680e6cf74dc75
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 21:20:33 +0200

    Clase Niño

--- entregas/orejaPaula/src/Niño.java
+++ entregas/orejaPaula/src/Niño.java
@@ -1,23 +1,26 @@
 public class Niño {
     private String nombre;
     private int edad;
-}
-public Niño(String nombre, int edad) {
-    this.nombre = nombre;
-    this.edad = edad;
-}
-public String getNombre() {
-    return nombre;
-}
 
-public int getEdad() {
-    return edad;
-}
-public String presentarse() {
-    return "Hola, soy " + nombre + " y tengo " + edad + " años";
-}
-@Override
-public String toString() {
-    return nombre + " (" + edad + " años)";
-}
+    public Niño(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String presentarse() {
+        return "Hola, soy " + nombre + " y tengo " + edad + " años";
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + edad + " años)";
+    }
+}
