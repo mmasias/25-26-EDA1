71e3381b0b6f137ae6447ec60bbe3bfd57f42fe1
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:55:22 +0200

    Clase Dalsy

--- entregas/orejaPaula/src/Dalsy.java
+++ entregas/orejaPaula/src/Dalsy.java
@@ -1,3 +1,4 @@
+```java
 import java.util.LinkedList;
 
 public class Dalsy {
@@ -8,29 +9,35 @@ public class Dalsy {
         this.nombre = "Dalsy";
         this.cola = new LinkedList<>();
     }
-}
-public void recibirNiño(Niño n) {
-    cola.add(n);
-}
-public LinkedList<Niño> getCola() {
-    return cola;
-}
 
-public int contarNiños() {
-    return cola.size();
-}
-public void mostrarCola() {
-    if (cola.isEmpty()) {
-        System.out.println("  Cola vacía");
-    } else {
-        System.out.println("  Niños en cola: " + cola.size());
-        for (Niño n : cola) {
-            System.out.println("   - " + n);
+    public void recibirNiño(Niño n) {
+        cola.add(n);
+    }
+
+    public LinkedList<Niño> getCola() {
+        return cola;
+    }
+
+    public int contarNiños() {
+        return cola.size();
+    }
+
+    public void mostrarCola() {
+        if (cola.isEmpty()) {
+            System.out.println("  Cola vacía");
+        } else {
+            System.out.println("  Niños en cola: " + cola.size());
+            for (Niño n : cola) {
+                System.out.println("   - " + n);
+            }
+        }
+    }
+
+    public void transferirTodosA(Lidia lidia) {
+        while (!cola.isEmpty()) {
+            lidia.recibirNiño(cola.poll());
         }
     }
 }
-public void transferirTodosA(Lidia lidia) {
-    while (!cola.isEmpty()) {
-        lidia.recibirNiño(cola.poll());
-    }
-}
+```
+
