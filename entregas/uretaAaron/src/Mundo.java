e1e3d8cbced52ce0c9404a291952fb2736a6609d
AaronUreta <aaron.ureta@uneatlantico.es>
2025-10-06 20:26:48 +0200

    menú interactivo de consola para la simulación

--- entregas/uretaAaron/src/Mundo.java
+++ entregas/uretaAaron/src/Mundo.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Mundo {
-    
-}
+    private final Ludoteca ludoteca; 
+    private final Scanner sc;        
+
+    public Mundo() {
+        this.ludoteca = new Ludoteca(); 
+        this.sc = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) { 
+        new Mundo().ejecutarSimulacion();
+    }
+
+    public void ejecutarSimulacion() { 
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerEntero("Elige una opción: "); 
+            System.out.println();
+            procesarOpcion(opcion);
+            if (opcion != 0) {
+                System.out.print("Pulsa ENTER para continuar..."); 
+                sc.nextLine();
+                System.out.println();
+            }
