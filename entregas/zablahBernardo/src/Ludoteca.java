06d1d2d5549d13ae364676f1c7333a81220272f2
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-10-06 23:13:21 +0200

    afinando detalles, creo que ya terminado

--- entregas/zablahBernardo/src/Ludoteca.java
+++ entregas/zablahBernardo/src/Ludoteca.java
@@ -1,38 +1,46 @@
-
-
 import utils.Console;
 
 class Ludoteca {
     private Monitor lydia;
     private Monitor aisha;
+    private Monitor dalsy;
 
-
-    public Ludoteca(){
+    public Ludoteca() {
         lydia = new Monitor("Lydia");
         aisha = new Monitor("Aisha");
+        dalsy = new Monitor("Dalsy");
     }
 
-    public void recibirNiño(Niño niño) {
-        lydia.recibeNiño(niño);
+    public void recibirNiñoEnLydia(Niño niñoNuevo) {
+        lydia.recibirNiño(niñoNuevo);
+        transferirNiñosLydiaAAisha();
     }
 
-    public void actualizar() {
-        gestionarNiños();
-        if (aisha.puedeJugar()) {
-            aisha.jugar();
-        } 
+    private void transferirNiñosLydiaAAisha() {
+        Niño[] todosNiñosLydia = lydia.obtenerTodosNiños();
+        for (Niño niñoActual : todosNiñosLydia) {
+            aisha.recibirNiño(niñoActual);
+        }
+        lydia.vaciarCola();
     }
 
-    private void gestionarNiños() {
-        if (lydia.tieneNiños() && !aisha.estaJugando()) {
-            lydia.entregaNiños(aisha);
-        }        
+    public Monitor getAisha() {
+        return aisha;
     }
 
-    public void verEstado(){
-        lydia.mostrarListaNiños();
-        aisha.mostrarListaNiños();
-        
-        new Console().writeln ("Uso de arrays: Lydia " + lydia.getUsoMemoria() + ", Aisha " + aisha.getUsoMemoria());
+    public Monitor getLydia() {
+        return lydia;
+    }
+
+    public Monitor getDalsy() {
+        return dalsy;
+    }
+
+    public void mostrarEstado(Console console) {
+        console.writeln("===== ESTADO DE LA LUDOTECA =====");
+        lydia.mostrarListaNiños(console);
+        aisha.mostrarListaNiños(console);
+        dalsy.mostrarListaNiños(console);
+        console.writeln("=================================");
     }
 }
