8327d92f49f21ed6ca638abb112322e58b7cb828
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:58:15 +0200

    Añadir método finalizarJuego para devolver los niños a Aisha

--- entregas/orejaPaula/src/JuegoRana.java
+++ entregas/orejaPaula/src/JuegoRana.java
@@ -14,3 +14,10 @@ public void iniciarJuego() {
     aisha.separarParaJuego(dalsy);
     System.out.println("Juego de la rana en progreso...");
 }
+public void finalizarJuego() {
+    for (Niño n : dalsy.getCola()) {
+        if (n.getEdad() < 5) aisha.recibirNiño(n);
+    }
+    dalsy.getCola().removeIf(n -> n.getEdad() < 5);
+    System.out.println("Juego de la rana terminado, todos los niños vuelven a Aisha");
+}
