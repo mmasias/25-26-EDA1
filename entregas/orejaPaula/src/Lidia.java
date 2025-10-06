3c59528b885685f1307f09ef8726019a58d9c2d9
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-10-06 20:59:46 +0200

    Crear clase Lidia con atributos y constructor básico

---
+++ entregas/orejaPaula/src/Lidia.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Lidia {
+    private String nombre;
+    private LinkedList<Niño> cola;
+
+    public Lidia() {
+        this.nombre = "Lidia";
+        this.cola = new LinkedList<>();
+    }
+}
+
