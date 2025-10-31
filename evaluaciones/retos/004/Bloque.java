78dd533484acd6017c248648701a29da7713c01b
elhadjiniang646 <niangbamba811@gmail.com>
2025-10-31 13:29:39 +0100

    impletacion del codigo

---
+++ evaluaciones/retos/004/Bloque.java
@@ -0,0 +1,46 @@
+package 004;
+
+class Bloques{
+    private final ArrayEnteros arrayInterno;
+
+    public Bloque(int capacidad) {
+        this.arrayInterno = new ArrayEnteros(capacidad);
+    }
+
+    public boolean estaLleno() {
+        return arrayInterno.estaLleno();
+    }
+
+    public boolean estaVacio() {
+        return arrayInterno.estaVacio();
+    }
+
+    public int usados() {
+        return arrayInterno.totalUsados();
+    }
+
+    public void agregarAlFinal(int valor) {
+        arrayInterno.agregarAlFinal(valor);
+    }
+
+    public int obtener(int indice) {
+        return arrayInterno.obtener(indice);
+    }
+
+    public void reemplazar(int indice, int valor) {
+        arrayInterno.reemplazar(indice, valor);
+    }
+
+    public void eliminarEn(int indice) {
+        arrayInterno.eliminarEn(indice);
+    }
+
+    public int eliminarPrimero() {
+        return arrayInterno.eliminarPrimero();
+    }
+
+    public int[] obtenerDatos() {
+        return arrayInterno.obtenerDatos();
+    }
+}
+    
