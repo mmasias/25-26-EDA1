bf8971bd59ac3ed46e59a6c31d7d9accec0e7fc1
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-10-31 17:51:28 +0100

    feat: add methods to create DataSet with specified capacity and default constructor

--- entregas/figueroaMarco/src/ArrayConstructor.java
+++ entregas/figueroaMarco/src/ArrayConstructor.java
@@ -1,3 +1,10 @@
 public class ArrayConstructor {
     
+    public DataSet crearArray(int capacity) {
+        return new DataSet(capacity);
+    }
+    
+    public DataSet crearArray() {
+        return new DataSet();
+    }
 }
