66ceecfc48b52a0c3fe64b858ecadb33651b2014
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-10-31 15:37:50 +0100

    Implementación inicial de la clase Eliminar

--- entregas/figueroaMarco/src/Eliminar.java
+++ entregas/figueroaMarco/src/Eliminar.java
@@ -1,3 +1,15 @@
 public class Eliminar {
     
+    public static void eliminarInicio(ArrayConstructor constructor) {
+        if (constructor.getCabeza() == -1){
+            System.out.println("Error: lista vacía");
+            return;
+        }
+        
+        int indiceEliminar = constructor.getCabeza();
+        constructor.setCabeza(constructor.getReferencias()[indiceEliminar]);
+        
+
+    }
+
 }
