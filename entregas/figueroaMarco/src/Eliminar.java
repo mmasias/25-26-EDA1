e11b542aefb3bcf886b0312e6a87c73b42f61b5e
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-10-31 16:09:01 +0100

    feat: implementa métodos eliminarInicio y eliminarFinal completos

--- entregas/figueroaMarco/src/Eliminar.java
+++ entregas/figueroaMarco/src/Eliminar.java
@@ -1,18 +1,48 @@
-public class Eliminar {
+class Eliminar {
     
     public static void eliminarInicio(ArrayConstructor constructor) {
-        if (constructor.getCabeza() == -1){
-            System.out.println("Error: lista vacía");
+        if (constructor.getCabeza() == -1) {
+            System.out.println("Error: Lista vacía");
             return;
         }
         
         int indiceEliminar = constructor.getCabeza();
+        
         constructor.setCabeza(constructor.getReferencias()[indiceEliminar]);
+        
         constructor.getArray()[indiceEliminar] = null;
+        
         constructor.getReferencias()[indiceEliminar] = constructor.getPrimerLibre();
         constructor.setPrimerLibre(indiceEliminar);
+        
         constructor.decrementarTamaño();
-
     }
-
+    
+    public static void eliminarFinal(ArrayConstructor constructor) {
+        if (constructor.getCabeza() == -1) {
+            System.out.println("Error: Lista vacía");
+            return;
+        }
+        
+        if (constructor.getReferencias()[constructor.getCabeza()] == -1) {
+            eliminarInicio(constructor);
+            return;
+        }
+        
+        int anterior = constructor.getCabeza();
+        int actual = constructor.getReferencias()[anterior];
+        
+        while (constructor.getReferencias()[actual] != -1) {
+            anterior = actual;
+            actual = constructor.getReferencias()[actual];
+        }
+        
+        constructor.getReferencias()[anterior] = -1;
+        constructor.getArray()[actual] = null;
+        
+        constructor.getReferencias()[actual] = constructor.getPrimerLibre();
+        constructor.setPrimerLibre(actual);
+        
+        constructor.decrementarTamaño();
+    }
 }
