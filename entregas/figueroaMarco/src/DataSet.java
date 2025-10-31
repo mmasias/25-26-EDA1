0343802283ecc5ee54cafa5c83e8bd1eed6d6ea3
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-10-31 13:31:39 +0100

    fix: remove unused datatype field from DataSet class

--- entregas/figueroaMarco/src/DataSet.java
+++ entregas/figueroaMarco/src/DataSet.java
@@ -1,12 +1,10 @@
 public class DataSet {
     private Object[] data;
-    private String datatype;
     private int before;
     private int after;
     
     public DataSet(Object[] data, String datatype, int before, int after) {
         this.data = data;
-        this.datatype = datatype;
         this.before = before;
         this.after = after;
     }
