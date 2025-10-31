915c13f0cd8d30aa022367447b545347c14abc83
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-10-31 13:25:33 +0100

    feat: Implement DataSet class with constructors and properties

--- entregas/figueroaMarco/src/DataSet.java
+++ entregas/figueroaMarco/src/DataSet.java
@@ -1,1 +1,19 @@
+public class DataSet {
+    private Object[] data;
+    private String datatype;
+    private int before;
+    private int after;
+    
+    public DataSet(Object[] data, String datatype, int before, int after) {
+        this.data = data;
+        this.datatype = datatype;
+        this.before = before;
+        this.after = after;
+    }
+    
+    public DataSet() {
+      
+    }
+    
+}
 
