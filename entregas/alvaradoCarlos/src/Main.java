98390d6fe89c31227c47b4a8867e9c4a7dac963a
Carlos Fernando Alvarado Lima <carlos.alvarado@alumnos.uneatlantico.es>
2025-11-10 12:51:57 +0000

    Mejora de main e implementación

--- entregas/alvaradoCarlos/src/Main.java
+++ entregas/alvaradoCarlos/src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         ArraySimulado miArray = new ArraySimulado(5);
-        System.out.println("Array inicial: " + miArray);
+        System.out.println("Array: " + miArray);
 
         miArray.set(0, 1);
         miArray.set(1, 2);
+        miArray.set(2, 1);
+        miArray.set(3, 2);
 
-        System.out.println("Array inicial: " + miArray);
+        System.out.println("Array: " + miArray);
 
-        miArray.get(0);
         System.out.println(miArray.get(0));
         System.out.println(miArray.get(1));
     }
