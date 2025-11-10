e92fd4069f8d60676bc6f121396379eb5a84f2fb
bienvee@uneat <bienvenido.doalto@alumnos.uneatlantico.es>
2025-11-10 13:09:00 +0100

    feat: implementacion codigo antiguo(lista usando array)

---
+++ entregas/doaltoBienvenido/src/Principal.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ListaEnteros listaEnteros = new ListaEnteros();
+        int opcion;
+
+        do {
+            System.out.println("\n1. Insertar\n2. Eliminar\n3. Obtener\n4. Mostrar\n0. Salir");
+            System.out.print("Ingrese opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    listaEnteros.mostrarPosicionesValidas();
+                    System.out.print("Ingrese valor a insertar: ");
+                    int valor = scanner.nextInt();
+                    System.out.print("Ingrese posición: ");
+                    int posicion = scanner.nextInt();
+                    listaEnteros.insertar(valor, posicion);
+                    break;
+                case 2:
+                    System.out.print("Ingrese posición a eliminar: ");
+                    int posicionEliminar = scanner.nextInt();
+                    listaEnteros.eliminar(posicionEliminar);
+                    break;
+                case 3:
+                    System.out.print("Ingrese posición a obtener: ");
+                    int posicionObtener = scanner.nextInt();
+                    int valorObtenido = listaEnteros.obtener(posicionObtener);
+                    if (valorObtenido != -1) {
+                        System.out.println("Valor: " + valorObtenido);
+                    }
+                    break;
+                case 4:
+                    listaEnteros.mostrar();
+                    break;
+                case 0:
+                    System.out.println("Fin del programa.");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
+
+        scanner.close();
+    }
+}
