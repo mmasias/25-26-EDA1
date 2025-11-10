375f42917382e7cc0767da8f99652d7e6165e7bb
Carlos Fernando Alvarado Lima <carlos.alvarado@alumnos.uneatlantico.es>
2025-11-10 12:46:01 +0000

    Refactor de main para pruebas del array simulado

--- entregas/alvaradoCarlos/src/Main.java
+++ entregas/alvaradoCarlos/src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        ArraySimulado array = new ArraySimulado(3);
 
-        System.out.println("Agregando elementos...");
-        array.añadir(10);
-        array.añadir(20);
-        array.añadir(30);
-        array.añadir(45);
-        array.imprimir();
-        System.out.println("Modificando índice 1...");
-        array.modificar(1, 5);
-        array.imprimir();
+        ArraySimulado miArray = new ArraySimulado(5);
+        System.out.println("Array inicial: " + miArray);
+
+        miArray.set(0, 1);
+        miArray.set(1, 2);
+
+        System.out.println("Array inicial: " + miArray);
+
+        miArray.get(0);
+        System.out.println(miArray.get(0));
+        System.out.println(miArray.get(1));
     }
 }
