7da250b85abfee4a249332b488d4112c5a8901cb
Manuel-Munoz <manuel.munoz2@alumnos.uneatlantico.es>
2025-11-13 10:35:42 +0000

    fix/codigo de ejecucion de lista

--- entregas/munozManuel/src/Main.java
+++ entregas/munozManuel/src/Main.java
@@ -2,36 +2,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Lista lista = new Lista();
-        lista.imprimirListaDetallada();
         lista.agregarFinal(new Nodo(10));
-        lista.imprimirListaDetallada();
         lista.agregarFinal(new Nodo('c'));
-        System.out.println("Posicion 2 con dato c\n");
         lista.agregarFinal(new Nodo("nodo aleatorio"));
         lista.agregarFinal(new Nodo(true));
         lista.agregarFinal(new Nodo(10.22));
         lista.agregarInicio(new Nodo('b'));
+
+        lista.imprimirLista();
+        System.out.println("Lista detallada despues de agregar datos\n");
+        lista.imprimirListaDetallada();
         
         lista.eliminar(3);
-        lista.imprimirListaDetallada();
+        lista.imprimirLista();
         System.out.println("Posicion 3 eliminado\n");
+        lista.imprimirListaDetallada();
+        
         lista.insertar(new Nodo(45), 3);
-
         lista.imprimirLista();
         System.out.println("Lista detallada despues de insertar en posicion 3\n");
         lista.imprimirListaDetallada();
 
-        lista.imprimirLista();
-        System.out.println("Lista detallada despues de insertar al inicio o al final\n");
-        lista.imprimirListaDetallada();
-        
-
-        lista.imprimirLista();
-        System.out.println("Lista detallada despues de eliminar\n");
-        lista.imprimirListaDetallada();
-
         lista.actualizar("dato 3", 3);
-
         lista.imprimirLista();
         System.out.println("Lista detallada despues de actualizar en posicion 3\n");
         lista.imprimirListaDetallada();
@@ -43,9 +35,7 @@ public class Main {
         System.out.println("Lista detallada despues de buscar nodo en posicion 4\n");
         lista.imprimirListaDetallada();
 
-
         lista.actualizar(50, 4);
-
         lista.imprimirLista();
         System.out.println("Lista detallada despues de actualizar dato en posicion 4\n");
         lista.imprimirListaDetallada();
