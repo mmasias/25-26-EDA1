5322b59fd33ae577e524a39b69bb5234a666f5ed
Carlos Fernando Alvarado Lima <carlos.alvarado@alumnos.uneatlantico.es>
2025-11-28 19:03:29 +0000

    Implementando clase Pedidos (vista publica propuesta)  y atributos publicos como nombres y tiempos

---
+++ entregas/alvaradoCarlos/src/Pedidos.java
@@ -0,0 +1,41 @@
+class Pedido {
+    String nombre;
+    int tiempoTotal;
+    int tiempoRestante;
+
+    public Pedido(TipoPlato plato) {
+        this.nombre = plato.nombre();
+        this.tiempoTotal = plato.calcularTiempoReal();
+        this.tiempoRestante = this.tiempoTotal;
+    }
+
+    public void cocinar() {
+        if (this.tiempoRestante > 0) {
+            this.tiempoRestante--;
+        }
+    }
+
+    public boolean estaTerminado() {
+        return this.tiempoRestante <= 0;
+    }
+
+    public boolean esMasUrgenteQue(Pedido otroPedido) {
+        return this.tiempoRestante < otroPedido.tiempoRestante;
+    }
+
+    public String nombrePlato() {
+        return nombre;
+    }
+
+    public int tiempoRestante() {
+        return tiempoRestante;
+    }
+
+    public int tiempoTotal() {
+        return tiempoTotal;
+    }
+
+    public String toString() {
+        return String.format("%s (%d min)", nombre, tiempoRestante);
+    }
+}
