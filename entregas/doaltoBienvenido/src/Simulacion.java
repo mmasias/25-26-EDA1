e32a1e5782fa7fca8b36cb5cbfc1ff137be7f7b9
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-11-29 22:28:27 +0100

    feat: eliminada implementacion de semilla random

--- entregas/doaltoBienvenido/src/Simulacion.java
+++ entregas/doaltoBienvenido/src/Simulacion.java
@@ -1,10 +1,8 @@
 public class Simulacion {
-
-    private static final double SEMILLA_ALEATORIA = 1234.0;
     private static final int DURACION_SIMULACION = 200;
 
     public static void main(String[] args) {
-        Restaurante restaurante = new Restaurante(SEMILLA_ALEATORIA);
+        Restaurante restaurante = new Restaurante();
         restaurante.ejecutar(DURACION_SIMULACION);
     }
 }
