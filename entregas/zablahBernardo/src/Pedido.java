b05b3d1b7fa14babd02a4c1350b52dbea86ab793
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-11-30 17:54:12 +0100

    Clase Pedido

--- entregas/zablahBernardo/src/Pedido.java
+++ entregas/zablahBernardo/src/Pedido.java
@@ -1,4 +1,33 @@
 
 public class Pedido {
+    private static int contadorId = 1;
+    
+    private int id;
+    private String nombre;
+    private int tiempoPreparacion;
+    private int tiempoRestante;
+    private int minutoLlegada;
+
+    public Pedido(String nombre, int tiempo, int minutoActual) {
+        this.id = contadorId++;
+        this.nombre = nombre;
+        this.tiempoPreparacion = tiempoPreparacion;
+        this.tiempoRestante = tiempoPreparacion;
+        this.minutoLlegada = minutoLlegada;
+    }
+
+    public void cocinarUnMinuto() {
+        tiempoRestante--;
+    }
+
+    public boolean estaTerminado() {
+        return tiempoRestante <= 0;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getTiempoPreparacion() { return tiempoPreparacion; }
+    public int getTiempoRestante() { return tiempoRestante; }
+    public int getMinutoLlegada() { return minutoLlegada; }
+    public int getId() { return id; }
 
 }
