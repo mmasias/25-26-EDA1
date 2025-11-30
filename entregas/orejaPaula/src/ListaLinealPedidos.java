6dc1c73c924a2461a64ccd8683dc8d5756979fdf
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:41:28 +0100

    Añadir atributos lista de pedidos y contador de comparaciones

--- entregas/orejaPaula/src/ListaLinealPedidos.java
+++ entregas/orejaPaula/src/ListaLinealPedidos.java
@@ -1,3 +1,8 @@
-public class ListaLinealPedidos {
-    
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaLinealPedidos implements IEstructuraPedidos {
+    private final List<Pedido> lista;
+    private long comparaciones;
 }
+
