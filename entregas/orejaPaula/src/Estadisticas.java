f98af6623a5b3edcbb151b6a03af6d0feb04c992
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:22:52 +0100

    Añadir getters y cálculo del tiempo medio de espera

--- entregas/orejaPaula/src/Estadisticas.java
+++ entregas/orejaPaula/src/Estadisticas.java
@@ -17,7 +17,31 @@ public class Estadisticas {
     public void setPedidosPendientes(int n) {
         pedidosPendientes = n;
     }
+
+    public int getPedidosAtendidos() {
+        return pedidosAtendidos;
+    }
+
+    public int getPedidosPendientes() {
+        return pedidosPendientes;
+    }
+
+    public long getTiempoTotalEspera() {
+        return tiempoTotalEspera;
+    }
+
+    public long getComparacionesTotales() {
+        return comparacionesTotales;
+    }
+
+    public double getTiempoMedioEspera() {
+        if (pedidosAtendidos == 0) {
+            return 0.0;
+        }
+        return (double) tiempoTotalEspera / pedidosAtendidos;
+    }
 }
+
 
 
 
