466277b042a90f721216af049e469c8c744347bb
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-11-30 22:12:35 +0100

    feat/creacion de clientes

--- entregas/munozManuel/src/Cliente.java
+++ entregas/munozManuel/src/Cliente.java
@@ -1,3 +1,19 @@
-public class Cliente {
-    
+package entregas.munozManuel.src;
+
+class Cliente {
+    private final String nombre;
+    private final Pedido pedido;
+
+    public Cliente(String nombre){
+        this.nombre = nombre;
+        pedido = new Pedido();
+    }
+
+    public String nombre(){
+        return nombre;
+    }
+
+    public Pedido darPedido(){
+        return pedido;
+    }
 }
