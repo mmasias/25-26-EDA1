60b73e1b0215753d49d9b310ca618d3e94eb81f9
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 13:30:31 +0100

    Crear clase Pedido con atributos principales

--- entregas/orejaPaula/src/Pedido.java
+++ entregas/orejaPaula/src/Pedido.java
@@ -1,3 +1,9 @@
 public class Pedido {
-    
+    private final int id;
+    private final Plato plato;
+    private final int tiempoPreparacion;
+    private int tiempoRestante;
+    private final int instanteLlegada;
+    private int instanteInicio;
 }
+
