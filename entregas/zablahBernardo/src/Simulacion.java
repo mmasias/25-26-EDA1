a64fe1e4c74f95bdf3b2828219bf0c79d6b47a43
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-11-30 16:56:06 +0100

    Creacion de clases.

--- entregas/zablahBernardo/src/Simulacion.java
+++ entregas/zablahBernardo/src/Simulacion.java
@@ -1,5 +1,27 @@
+import java.util.Random;
 
 public class Simulacion {
+    private static final int JORNADA_MINUTOS = 720;
+    private static final double PROBABILIDAD_LLEGADA = 0.4;
+
+    private ArbolPedidos arbolPedidos;
+    private PresentacionConsola presentacionConsola;
+    private Random random;
+    private Pedido pedido;
+
+    private int pedidosAtendidos;
+    private int tiempoEsperaTotal;
+
+    public Simulacion() {
+        this.arbolPedidos = new ArbolPedidos();
+        this.presentacionConsola = new PresentacionConsola();
+        this.random = new Random();
+        this.pedido = null;
+        
+        this.pedidosAtendidos = 0;
+        this.tiempoEsperaTotal = 0;
+    }
+
 
     public void start() {
         // TODO Auto-generated method stub
