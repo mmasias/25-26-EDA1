6e2a388471697f78f4195a468f11e3b99622bfe2
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 13:10:27 +0100

    Crear enum Plato con nombre y rango de tiempo

--- entregas/orejaPaula/src/Plato.java
+++ entregas/orejaPaula/src/Plato.java
@@ -1,3 +1,17 @@
-public class Plato {
-    
+public enum Plato {
+    BEBIDA("Bebida", 1, 2),
+    CAFE("Café", 2, 3),
+    COLACAO("Colacao", 2, 4),
+    BOCADILLO("Bocadillo", 3, 5),
+    ENSALADA("Ensalada", 5, 8);
+
+    private final String nombre;
+    private final int minTiempo;
+    private final int maxTiempo;
+
+    Plato(String nombre, int minTiempo, int maxTiempo) {
+        this.nombre = nombre;
+        this.minTiempo = minTiempo;
+        this.maxTiempo = maxTiempo;
+    }
 }
