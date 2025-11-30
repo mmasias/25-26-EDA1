fb48dcaf8675b1f866c68420ae468a9e7b974305
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:38:39 +0100

    Implementar constructor con Comparator para ordenar por menor tiempo de preparación

--- entregas/orejaPaula/src/ColaPrioridadPedidos.java
+++ entregas/orejaPaula/src/ColaPrioridadPedidos.java
@@ -1,7 +1,20 @@
 import java.util.PriorityQueue;
+import java.util.Comparator;
 
 public class ColaPrioridadPedidos implements IEstructuraPedidos {
     private final PriorityQueue<Pedido> cola;
     private long comparaciones;
+
+    public ColaPrioridadPedidos() {
+        cola = new PriorityQueue<>(new Comparator<Pedido>() {
+            @Override
+            public int compare(Pedido p1, Pedido p2) {
+                comparaciones++;
+                return Integer.compare(p1.getTiempoPreparacion(), p2.getTiempoPreparacion());
+            }
+        });
+        comparaciones = 0;
+    }
 }
+
 
