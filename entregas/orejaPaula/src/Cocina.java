411d253b08b45457ec685485950f4b45f35202ed
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:47:31 +0100

    Añadir constructor de Cocina

--- entregas/orejaPaula/src/Cocina.java
+++ entregas/orejaPaula/src/Cocina.java
@@ -1,5 +1,10 @@
 public class Cocina {
     private final IEstructuraPedidos estructuraPedidos;
     private Pedido pedidoActual;
+
+    public Cocina(IEstructuraPedidos estructuraPedidos) {
+        this.estructuraPedidos = estructuraPedidos;
+        this.pedidoActual = null;
+    }
 }
 
