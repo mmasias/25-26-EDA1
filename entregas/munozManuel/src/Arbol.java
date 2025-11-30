5d4fe1f3a0f7a5cf182552a8532ffc2c692be000
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-11-30 22:06:26 +0100

    feat/recorrer el arbol e imprimirlo

--- entregas/munozManuel/src/Arbol.java
+++ entregas/munozManuel/src/Arbol.java
@@ -1,22 +1,48 @@
+package entregas.munozManuel.src;
+
 class Arbol{
     private Nodo raiz;
+    private int cantidadNodos = 0;
 
-    public void insertarNodo(Nodo nodo){
+    public void insertarPedido(Pedido pedido){
         if (raiz == null) {
-            raiz = nodo;
+            raiz = new Nodo(pedido);
+            cantidadNodos++;
         }else{
-            if(raiz.tiempoPreparacion() >= nodo.tiempoPreparacion()){
-                raiz.hijoIzquerda(nodo);
-            }else{
-                raiz.hijoDerecha(nodo);
+            raiz.insertarHijo(new Nodo(pedido));
+            cantidadNodos++;
+        }
+    }
+
+    public void recorrerArbol() {
+        if (raiz == null || cantidadNodos == 0) {
+            System.out.println("Árbol vacío");
+            return;
+        }
+
+        Nodo[] nodosOrdenados = new Nodo[cantidadNodos];
+        int indiceInicial = 0;
+
+        recorrerInOrden(raiz, nodosOrdenados, indiceInicial);
+
+        for (Nodo nodosOrdenado : nodosOrdenados) {
+            if (nodosOrdenado != null) {
+                System.out.println(nodosOrdenado.nombrePedido() + " elemnto con timepo: " + nodosOrdenado.tiempoPreparacion());
             }
         }
     }
 
-    private void recorerArbol(){
+    private int recorrerInOrden(Nodo nodo, Nodo[] arreglo, int indice) {
+        if (nodo == null) return indice;
+
+        indice = recorrerInOrden(nodo.hijoIzquerda(), arreglo, indice);
+
+        arreglo[indice] = nodo;
+        indice += 1;
+
+        indice = recorrerInOrden(nodo.hijoDerecha(), arreglo, indice);
+
+        return indice;
     }
 
-    private void organizarArbol(){
-
-    }
 }
