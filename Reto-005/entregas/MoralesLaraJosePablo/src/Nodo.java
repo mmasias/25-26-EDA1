c82f88bad511a8dcb4638a028591f04ae099cbc9
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-11-30 23:07:39 +0100

    Agrego la clase Nodo para estructura del árbol binario

---
+++ Reto-005/entregas/MoralesLaraJosePablo/src/Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo {
+
+    Pedido pedido;
+    Nodo izquierdo;
+    Nodo derecho;
+    
+    public Nodo (Pedido pedido) {
+        this.pedido = pedido;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
