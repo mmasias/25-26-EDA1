c49dc0ca46a5f044b8b014212016a62b909d7fc7
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:42:59 +0100

    Implementar extraerMinimo() con búsqueda lineal y conteo de comparaciones

--- entregas/orejaPaula/src/ListaLinealPedidos.java
+++ entregas/orejaPaula/src/ListaLinealPedidos.java
@@ -24,7 +24,29 @@ public class ListaLinealPedidos implements IEstructuraPedidos {
     public int tamano() {
         return lista.size();
     }
+
+    @Override
+    public Pedido extraerMinimo() {
+        if (lista.isEmpty()) {
+            return null;
+        }
+        int indiceMinimo = 0;
+        Pedido minimo = lista.get(0);
+
+        for (int i = 1; i < lista.size(); i++) {
+            Pedido actual = lista.get(i);
+            comparaciones++;
+            if (actual.getTiempoPreparacion() < minimo.getTiempoPreparacion()) {
+                minimo = actual;
+                indiceMinimo = i;
+            }
+        }
+
+        lista.remove(indiceMinimo);
+        return minimo;
+    }
 }
+
 
 
 
