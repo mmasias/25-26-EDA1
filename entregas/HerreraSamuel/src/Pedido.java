03a374451ad4d6796a4eadbf770499fc8d2251c3
samyx <samyxelita@gmail.com>
2025-11-30 23:10:00 +0100

    Simulacion y vista

--- entregas/HerreraSamuel/src/Pedido.java
+++ entregas/HerreraSamuel/src/Pedido.java
@@ -1,12 +1,14 @@
 public class Pedido {
     private String tipoDePlato;
     private int tiempoDePreparacion;
+    private int minutoLlegada;
     
 
-    public Pedido (String tipoDePlato, int tiempoDePreparacion)
+    public Pedido (String tipoDePlato, int tiempoDePreparacion,int minutoLlegada)
     {
         this.tiempoDePreparacion = tiempoDePreparacion;
         this.tipoDePlato = tipoDePlato;
+        this.minutoLlegada = minutoLlegada;
     }
 
     public String getTipoDePlato() {
@@ -15,6 +17,10 @@ public class Pedido {
 
     public int getTiempoDePreparacion(){
         return tiempoDePreparacion;
+    }
+    
+    public int getMinutoLlegada() {
+        return minutoLlegada;
     }
  
 }
