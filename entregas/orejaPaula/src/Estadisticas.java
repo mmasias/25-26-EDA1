b3d7b82a2fe8acc77ce2ee907092917324e53171
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:16:42 +0100

    Crear clase Estadisticas con contadores principales

--- entregas/orejaPaula/src/Estadisticas.java
+++ entregas/orejaPaula/src/Estadisticas.java
@@ -1,3 +1,7 @@
 public class Estadisticas {
-    
+    private int pedidosAtendidos;
+    private int pedidosPendientes;
+    private long tiempoTotalEspera;
+    private long comparacionesTotales;
 }
+
