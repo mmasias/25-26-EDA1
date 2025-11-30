d40b83caa82ec816cd766156e036ef6d456c2009
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:53:05 +0100

    Crear clase Simulation con atributos de configuración y estado

--- entregas/orejaPaula/src/Simulation.java
+++ entregas/orejaPaula/src/Simulation.java
@@ -1,3 +1,11 @@
 public class Simulation {
-    
+
+    private final int duracionJornada;
+    private final double probLlegada;
+    private int tiempoActual;
+    private final Cocina cocina;
+    private final Estadisticas estadisticas;
+    private final Random random;
+    private int siguienteId;
 }
+
