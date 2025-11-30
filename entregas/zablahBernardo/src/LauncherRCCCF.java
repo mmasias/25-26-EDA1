1f8590abae301b73c158e1a8cb6e3d7ec6925c4c
BernardoZablah <bernardo.zablah@alumnos.uneatltantico.es>
2025-11-30 16:35:00 +0100

    Creacion clase Launcher y clase principal de simulaicon

---
+++ entregas/zablahBernardo/src/LauncherRCCCF.java
@@ -0,0 +1,6 @@
+public class LauncherRCCCF {
+        public static void main(String[] args) {
+        Simulacion simulacion = new Simulacion();
+        simulacion.start();
+    }
+}
---
+++ entregas/zablahBernardo/src/Simulacion.java
@@ -0,0 +1,9 @@
+
+public class Simulacion {
+
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+}
