a736f9db8cc18b634b2625b1684880cea11be244
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-11-30 22:08:59 +0100

    feat/pedidos aleatorios

--- entregas/munozManuel/src/Pedido.java
+++ entregas/munozManuel/src/Pedido.java
@@ -1,18 +1,29 @@
-class Pedido {
-    private final String nombre;
-    private final int tiempoPreparacion;
+package entregas.munozManuel.src;
 
-    public Pedido(String nombre, int tiempoPreparacion) {
-        this.nombre = nombre;
-        this.tiempoPreparacion = tiempoPreparacion;
+class Pedido {
+    private final String pedido;
+    private final int tiempoPreparacion;
+    private final String[] PEDIDOS_DISPONIBLES = {"Bebida", "Café", "Colacao", "Bocadillo", "Ensalada"};
+    private final static int[][] TIEMPO_PREPARACION = {{1, 2}, {2, 3}, {2, 4}, {3, 5}, {5, 8}};
+
+    public Pedido() {
+        int numeroDePedido = (int)(Math.random() * 5);
+        pedido = PEDIDOS_DISPONIBLES[numeroDePedido];
+        tiempoPreparacion = calcularTiempoPreparacion(numeroDePedido);
     }
 
-    public String nombre() {
-        return nombre;
+    public String pedido() {
+        return pedido;
     }
 
     public int tiempoPreparacion() {
         return tiempoPreparacion;
+    }
+
+    private static int calcularTiempoPreparacion(int numeroDePedido){
+        int min = TIEMPO_PREPARACION[numeroDePedido][0];
+        int max = TIEMPO_PREPARACION[numeroDePedido][1];
+        return (int)(Math.random() * (max - min + 1)) + min;
     }
     
 }
