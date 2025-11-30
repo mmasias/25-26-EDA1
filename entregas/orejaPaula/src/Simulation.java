5be442c792cc1e94f9119ee4de8fe795f2ff7d14
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:56:59 +0100

    Añadir constructor de Simulation para inicializar cocina, estadísticas y valores iniciales

--- entregas/orejaPaula/src/Simulation.java
+++ entregas/orejaPaula/src/Simulation.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Simulation {
 
     private final int duracionJornada;
@@ -7,5 +9,16 @@ public class Simulation {
     private final Estadisticas estadisticas;
     private final Random random;
     private int siguienteId;
+
+    public Simulation(int duracionJornada, double probLlegada, IEstructuraPedidos estructura) {
+        this.duracionJornada = duracionJornada;
+        this.probLlegada = probLlegada;
+        this.tiempoActual = 0;
+        this.cocina = new Cocina(estructura);
+        this.estadisticas = new Estadisticas();
+        this.random = new Random();
+        this.siguienteId = 1;
+    }
 }
+
 
