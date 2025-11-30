b64e40ceff9c3ccee84f67ba257a4757e4edfe6a
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-11-30 23:49:22 +0100

    Implementación de Simulación

---
+++ entregas/lianoJavier/src/Simulador.java
@@ -0,0 +1,99 @@
+public class Simulador {
+
+        private static final int INICIO = 0;
+        private static final int EJECUTANDO = 1;
+        private static final int FINALIZADO = 2;
+
+        private Arbol colaPedidos;
+        private Cocinero cocinero;
+        private GeneradorDePedidos generador;
+        private EstadisticasJornada estadisticas;
+        private Reloj reloj;
+        private int estadoActual;
+
+        public Simulador() {
+                this.colaPedidos = new Arbol();
+                this.cocinero = new Cocinero();
+                this.generador = new GeneradorDePedidos();
+                this.estadisticas = new EstadisticasJornada();
+                this.reloj = new Reloj();
+                this.estadoActual = INICIO;
+        }
+
+        public static void main(String[] args) {
+                new Simulador().simular();
+        }
+
+        public void simular() {
+                boolean simulacionActiva = true;
+
+                while (simulacionActiva) {
+                        switch (estadoActual) {
+                                case INICIO -> estadoActual = EJECUTANDO;
+                                case EJECUTANDO -> {
+                                        ejecutarCiclo();
+                                        if (reloj.esFinDeJornada())
+                                                estadoActual = FINALIZADO;
+                                }
+                                case FINALIZADO -> {
+                                        finalizarJornada();
+                                        simulacionActiva = false;
+                                }
+                        }
+                }
+        }
+
+        private void ejecutarCiclo() {
+                reloj.avanzarMinuto();
+                int minuto = reloj.obtenerMinutoActual();
+
+                VisualizadorDeSimulacion.mostrarEncabezadoMinuto(minuto);
+
+                gestionarLlegadaPedidos(minuto);
+                gestionarCocina(minuto);
+
+                VisualizadorDeSimulacion.mostrarEstadoCocina(cocinero, colaPedidos, minuto);
+        }
+
+        private void gestionarLlegadaPedidos(int minuto) {
+                Pedido nuevoPedido = generador.intentarGenerar(minuto);
+
+                if (nuevoPedido == null)
+                        return;
+
+                colaPedidos.insertar(nuevoPedido);
+                VisualizadorDeSimulacion.mostrarLlegadaPedido(nuevoPedido, minuto);
+        }
+
+        private void gestionarCocina(int minuto) {
+                if (!cocinero.estaOcupado() && !colaPedidos.estaVacia())
+                        cocinero.tomarNuevoPedido(colaPedidos);
+
+                if (!cocinero.estaOcupado())
+                        return;
+
+                cocinero.procesarMinuto();
+
+                if (!cocinero.obtenerPedidoActual().estaCompletado())
+                        return;
+
+                Pedido pedidoCompletado = cocinero.finalizarPedidoActual();
+                estadisticas.registrarPedidoAtendido(pedidoCompletado, minuto);
+
+                if (!colaPedidos.estaVacia())
+                        cocinero.tomarNuevoPedido(colaPedidos);
+        }
+
+        private void finalizarJornada() {
+                contarPedidosPendientes();
+                estadisticas.actualizarComparaciones(colaPedidos.obtenerComparaciones());
+                VisualizadorDeSimulacion.mostrarResumen(estadisticas);
+        }
+
+        private void contarPedidosPendientes() {
+                while (!colaPedidos.estaVacia()) {
+                        colaPedidos.extraerMinimo();
+                        estadisticas.registrarPedidoPendiente();
+                }
+        }
+}
