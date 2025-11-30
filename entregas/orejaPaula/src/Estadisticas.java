3bcb26c1655cc314f12bbb7d536c1c927ee0b2ef
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-11-30 18:20:53 +0100

    Añadir métodos para registrar atención y sumar comparaciones

--- entregas/orejaPaula/src/Estadisticas.java
+++ entregas/orejaPaula/src/Estadisticas.java
@@ -3,5 +3,16 @@ public class Estadisticas {
     private int pedidosPendientes;
     private long tiempoTotalEspera;
     private long comparacionesTotales;
+
+    public void registrarAtencion(Pedido p) {
+        pedidosAtendidos++;
+        int espera = p.getInstanteInicio() - p.getInstanteLlegada();
+        tiempoTotalEspera += espera;
+    }
+
+    public void sumarComparaciones(long n) {
+        comparacionesTotales += n;
+    }
 }
+
 
