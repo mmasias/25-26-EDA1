94be265f8cda4e8f1d69fe071e0185a2daa079cb
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-12-01 13:32:48 +0100

    feat/cliente finalizado

--- entregas/munozManuel/src/Cliente.java
+++ entregas/munozManuel/src/Cliente.java
@@ -3,9 +3,21 @@ package entregas.munozManuel.src;
 class Cliente {
     private final String nombre;
     private final Pedido pedido;
+    private final String[] NOMBRE_CLIENTES = {
+    "Ana",
+    "Luis",
+    "María",
+    "Carlos",
+    "Sofía",
+    "Javier",
+    "Elena",
+    "Miguel",
+    "Laura",
+    "Andrés"
+};
 
-    public Cliente(String nombre){
-        this.nombre = nombre;
+    public Cliente(){
+        nombre = NOMBRE_CLIENTES[(int)(Math.random() * 9)];
         pedido = new Pedido();
     }
 
