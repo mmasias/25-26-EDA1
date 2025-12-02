ae944c4487df43b32469cf75cdc908968b3e5eae
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-12-02 23:12:12 +0100

    feat: implementados metodos en Console - imprimirln - saltoDeLinea - imprimir

--- entregas/lianoJavier/src/Console.java
+++ entregas/lianoJavier/src/Console.java
@@ -3,4 +3,17 @@ public class Console {
   public static int randomIntInRange(int range) {
     return (int) (Math.random() * range);
   }
+
+  public static void imprimirln(String string) {
+    imprimir(string);
+    saltoDeLinea();
+  }
+
+  public static void saltoDeLinea() {
+    System.out.println();
+  }
+
+  public static void imprimir(String string) {
+    System.out.print(string);
+  }
 }
