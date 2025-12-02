2f67513e1180edf612eefc5a2a7071d127185e63
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-12-02 22:13:39 +0100

    feat: cocinero decide cuanto un plato tarda He decidido que el cocinero es quien debería decidir cuanto tiempo tarda en prepararse un plato, ya que es él el que los cocina, aunque nunca saliendose del rango establecido. También es el cocinero quien sabe el tiempo que tarda cada plato en prepararse.

--- entregas/lianoJavier/src/Cocinero.java
+++ entregas/lianoJavier/src/Cocinero.java
@@ -4,6 +4,23 @@ public class Cocinero {
   private Arbol comandas;
   private Pedido comandaActual;
 
+  final static int[] TIEMPO_PREPARACION_BEBIDA = { 1, 2 };
+  final static int[] TIEMPO_PREPARACION_CAFE = { 2, 3 };
+  final static int[] TIEMPO_PREPARACION_COLACAO = { 2, 4 };
+  final static int[] TIEMPO_PREPARACION_BOCADILLO = { 3, 5 };
+  final static int[] TIEMPO_PREPARACION_ENSALADA = { 5, 8 };
+
+  private final static int[][] TIEMPO_PREPARACION_PLATOS = {
+      TIEMPO_PREPARACION_BEBIDA,
+      TIEMPO_PREPARACION_CAFE,
+      TIEMPO_PREPARACION_COLACAO,
+      TIEMPO_PREPARACION_BOCADILLO,
+      TIEMPO_PREPARACION_ENSALADA
+  };
+
+  private int tiempoCocinandoComanda;
+  private int tiempoDePreparacion;
+
   public Cocinero(Arbol comandas) {
     this.comandas = comandas;
   }
@@ -11,17 +28,27 @@ public class Cocinero {
   public void actualizar() {
     if (!estaCocinando()) {
       comandaActual = comandas.sacarMinimo();
+      tiempoCocinandoComanda = 0;
+      int rangoTiempoPlato = Math
+          .abs(TIEMPO_PREPARACION_PLATOS[comandaActual.getPlato()][1]
+              - TIEMPO_PREPARACION_PLATOS[comandaActual.getPlato()][0]);
+      tiempoDePreparacion = Console.randomIntInRange(rangoTiempoPlato);
     } else {
       cocinar();
     }
   }
 
   private void cocinar() {
-    comandaActual.actualizar();
+    assert tiempoCocinandoComanda >= tiempoDePreparacion : "[Cocinero]: ¿Me he pasado del tiempo de Preparación?";
 
-    if (comandaActual.terminada()) {
+    tiempoCocinandoComanda++;
+
+    if (haTerminada())
       entregarComanda();
-    }
+  }
+
+  private boolean haTerminada() {
+    return tiempoCocinandoComanda == tiempoDePreparacion;
   }
 
   private void entregarComanda() {
