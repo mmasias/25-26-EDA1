b7f8310935bc71466de138c0793a7fdc69b00934
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-12-02 22:28:23 +0100

    feat: Pedido Implementamos la logica de pedido. Para comparar si un plato es de menor tiempo que otro, utilizamos el valor maximo del tiempo de preparación Es decir, que si un cafe tiene 3 minutos maximos de espera y un Bocadillo 5, el cafe saldrá como menor.

--- entregas/lianoJavier/src/Pedido.java
+++ entregas/lianoJavier/src/Pedido.java
@@ -1,24 +1,37 @@
 
 public class Pedido {
 
-  private Pedido PlatoAleatorio() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'PlatoAleatorio'");
+  final static int BEBIDA = 1, CAFE = 2, COLACAO = 3, BOCADILLO = 4, ENSALADA = 5;
+  final static String[] NOMBRE_PLATOS = { "Bebida", "Cafe", "Colacao", "Bocadillo", "Ensalada" };
+
+  final static int[] TIEMPO_PREPARACION_BEBIDA = { 1, 2 };
+  final static int[] TIEMPO_PREPARACION_CAFE = { 2, 3 };
+  final static int[] TIEMPO_PREPARACION_COLACAO = { 2, 4 };
+  final static int[] TIEMPO_PREPARACION_BOCADILLO = { 3, 5 };
+  final static int[] TIEMPO_PREPARACION_ENSALADA = { 5, 8 };
+
+  private final static int[][] TIEMPO_PREPARACION_PLATOS = {
+      TIEMPO_PREPARACION_BEBIDA,
+      TIEMPO_PREPARACION_CAFE,
+      TIEMPO_PREPARACION_COLACAO,
+      TIEMPO_PREPARACION_BOCADILLO,
+      TIEMPO_PREPARACION_ENSALADA
+  };
+
+  private int plato;
+
+  public boolean esMenor(Pedido pedido) {
+    final int MAXIMO = 1;
+    return TIEMPO_PREPARACION_PLATOS[plato][MAXIMO] <= TIEMPO_PREPARACION_PLATOS[pedido.getPlato()][MAXIMO];
   }
 
-  private Pedido TiempoAleatorio() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'TiempoAleatorio'");
+  public Pedido platoAleatorio() {
+    this.plato = Console.randomIntInRange(NOMBRE_PLATOS.length);
+    return this;
   }
 
-  public void actualizar() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
-  }
-
-  public boolean terminada() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'terminada'");
+  public int getPlato() {
+    return plato;
   }
 
 }
