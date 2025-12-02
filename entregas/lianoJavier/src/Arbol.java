7bad64300b8e1b159408ff572edd48ddbbc7d5c3
midas@tower <javier.liano@alumnos.uneatlantico.es>
2025-12-02 23:56:41 +0100

    refact: alterada clase arbol - cambiado nombre de numero comanda a idNodo para ser más agnosticos - agregado la numeración de nodos

--- entregas/lianoJavier/src/Arbol.java
+++ entregas/lianoJavier/src/Arbol.java
@@ -2,22 +2,38 @@
 public class Arbol {
 
   private Nodo raiz;
-  private int numeroComanda;
+  private int idNodo;
+  private int numeroDeNodos;
+
+  public Arbol() {
+    numeroDeNodos = 0;
+    idNodo = 0;
+  }
 
   public void insertar(Pedido pedido) {
+    numeroDeNodos++;
     if (raiz == null)
-      raiz = new Nodo(pedido, numeroComanda);
+      raiz = new Nodo(pedido, idNodo);
     else {
-      raiz.insertar(pedido, numeroComanda);
+      raiz.insertar(pedido, idNodo);
     }
-    numeroComanda++;
+    idNodo++;
   }
 
   public Pedido sacarMinimo() {
+    numeroDeNodos--;
     Nodo nodoExtraido = raiz.sacarPedidoMinimo();
     if (raiz.esIgualNumeroComanda(nodoExtraido))
       raiz = raiz.sacarArbolMayor();
     return nodoExtraido.getPedido();
+  }
+
+  public int getNumeroNodos() {
+    return numeroDeNodos;
+  }
+
+  public boolean hayNodos() {
+    return numeroDeNodos != 0;
   }
 
 }
