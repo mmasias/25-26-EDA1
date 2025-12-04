75b4d0842073825851fc2a6cfa043ad26dfd521b
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-12-04 18:21:11 +0100

    rear estructura base de ArbolPedidos

--- entregas/orejaPaula/src/ArbolPedidos.java
+++ entregas/orejaPaula/src/ArbolPedidos.java
@@ -1,3 +1,14 @@
 public class ArbolPedidos {
-    
+
+    private static class Nodo {
+        Pedido p;
+        Nodo izq, der;
+
+        Nodo(Pedido p) {
+            this.p = p;
+        }
+    }
+
+    private Nodo raiz;
+    public int comparaciones;   
 }
