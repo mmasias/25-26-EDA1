dc0810307dc1b55089a17e0664b4e979bbb0baf4
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-12-04 20:59:08 +0100

    refactorización, para que nos muestre el resumen completo cuando la jornada termine

--- entregas/anayaAndrea/src/Cocinero.java
+++ entregas/anayaAndrea/src/Cocinero.java
@@ -1,18 +1,51 @@
 public class Cocinero {
 
-    private Pedido pedidoActual;
+    private ArbolPedidos arbol;
+    private Registro registro;
+    private Pedido actual;
+    private int tiempoEsperaTotal;
 
-    public boolean estaLibre() {
-        return pedidoActual == null;
+    public Cocinero() {
+        arbol = new ArbolPedidos();
+        registro = new Registro();
+        actual = null;
+        tiempoEsperaTotal = 0;
     }
 
-    public void asignarPedido(Pedido p) {
-        pedidoActual = p;
+    public void nuevoPedido(Pedido p) {
+        arbol.insertar(p);
     }
 
-    public Pedido procesarMinuto()
+    public void procesarMinuto(int minuto) {
 
-    public Pedido getPedidoActual() {
-        return pedidoActual;
-    
+        if (actual == null) {
+            if (!arbol.estaVacio()) {
+                actual = arbol.extraerMinimo();
+                System.out.println("[MINUTO " + minuto + "] Empieza: " + actual);
+            }
+        }
+
+        if (actual != null) {
+            actual.reducirTiempo();
+
+            if (actual.getTiempoRestante() == 0) {
+                System.out.println("[MINUTO " + minuto + "] Terminado: " + actual.getNombre());
+                registro.registrar(actual);
+                tiempoEsperaTotal += (minuto - actual.getMomentoLlegada());
+                actual = null;
+            }
+        }
+    }
+
+    public void resumenFinal() {
+        registro.mostrarResumen(
+                arbol.estaVacio() ? 0 : 1, 
+                arbol.getComparaciones(),
+                tiempoEsperaTotal
+        );
+    }
+
+    public int pedidosPendientes() {
+        return arbol.estaVacio() ? 0 : 1;
+    }
 }
