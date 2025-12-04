ecc295be1db737c1e450c2e578202d6c7c82d9b7
andrea.anaya <andrea.anaya@alumnos.uneatlantico.es>
2025-12-04 20:54:24 +0100

    añado más clases para organizar mejor el código

---
+++ entregas/anayaAndrea/src/Cocinero.java
@@ -0,0 +1,18 @@
+public class Cocinero {
+
+    private Pedido pedidoActual;
+
+    public boolean estaLibre() {
+        return pedidoActual == null;
+    }
+
+    public void asignarPedido(Pedido p) {
+        pedidoActual = p;
+    }
+
+    public Pedido procesarMinuto()
+
+    public Pedido getPedidoActual() {
+        return pedidoActual;
+    
+}
