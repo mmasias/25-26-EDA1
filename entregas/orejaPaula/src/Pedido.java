924df1e3579241f1b982ee2866c28ee347eed77a
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-12-04 18:17:43 +0100

    atributos básicos a clase Pedido

--- entregas/orejaPaula/src/Pedido.java
+++ entregas/orejaPaula/src/Pedido.java
@@ -1,4 +1,9 @@
 public class Pedido {
-  
+    public final String nombre;
+    public final int tiempoPreparacion;
+    public int tiempoRestante;
+    public final int llegada;
+    public int inicio;
 }
+
 
