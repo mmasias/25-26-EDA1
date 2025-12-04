f269d06b21bb1101ddb67d3137fff5ead2d0b15b
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-12-04 18:25:48 +0100

    Campos básicos

--- entregas/orejaPaula/src/Simulation.java
+++ entregas/orejaPaula/src/Simulation.java
@@ -1,3 +1,19 @@
+import java.util.Random;
+
 public class Simulation {
-    
+
+    private final ArbolPedidos arbol = new ArbolPedidos();
+    private Pedido actual = null;
+    private int atendidos = 0;
+    private int tiempoEspera = 0;
+
+    private final Random r = new Random();
+
+    public void iniciar() {
+    }
+
+    public static void main(String[] args) {
+        new Simulation().iniciar();
+    }
 }
+
