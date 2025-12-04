b46700602f1aa08306ada6987b92c24e116d939a
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-12-04 20:34:22 +0100

    Renombrar NodoPedido a NodoArbolPedido y adaptarlo para árbol binario añadiendo hijo izquierdo y derecho

---
+++ entregas/coboIsmael/src/NodoArbolPedido.java
@@ -0,0 +1,32 @@
+public class NodoArbolPedido {
+
+    private Pedido dato;
+    private NodoArbolPedido izquierdo;
+    private NodoArbolPedido derecho;
+
+    public NodoArbolPedido(Pedido dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public Pedido getDato() {
+        return dato;
+    }
+
+    public NodoArbolPedido getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoArbolPedido izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoArbolPedido getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoArbolPedido derecho) {
+        this.derecho = derecho;
+    }
+}
