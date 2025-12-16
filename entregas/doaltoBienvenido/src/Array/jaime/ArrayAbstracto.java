3a8ac0520ea0a962f6d0e8b41465cf37c64186cd
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-12-16 21:44:23 +0100

    deleted folder not usable

--- entregas/doaltoBienvenido/src/Array/jaime/ArrayAbstracto.java
+++
@@ -1,8 +0,0 @@
-package Array.jaime;
-public abstract class ArrayAbstracto {
-    protected NodoAbstracto primerNodo;
-    protected int NUMERO_MAXIMO;
-
-    public abstract NodoAbstracto get(int indice);
-    public abstract int size();
-}
